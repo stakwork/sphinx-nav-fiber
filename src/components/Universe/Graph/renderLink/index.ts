a08d99fa0271efc4848e0bdc30b42cdd246c25f1
emberist <13602797+emberist@users.noreply.github.com>
2022-09-15 22:32:53 +0200

    refactor: graph universe sprite instead of cubes

--- src/components/Universe/Graph/renderLink/index.ts
+++ src/components/Universe/Graph/renderLink/index.ts
@@ -1,13 +1,34 @@
 import * as THREE from "three";
-import { GRAPH_LINK_COLOR } from '../../../../constants'
+import { useDataStore } from "~/stores/useDataStore";
+import { GRAPH_LINK_COLOR } from "../../../../constants";
 
 const lineMaterial = new THREE.LineBasicMaterial({
   opacity: 0.5,
   transparent: true,
   color: GRAPH_LINK_COLOR,
   linewidth: 1,
+  depthWrite: false,
+});
+
+const selectedLineMaterial = new THREE.LineBasicMaterial({
+  opacity: 1,
+  transparent: true,
+  color: "yellow",
+  linewidth: 5,
+  depthWrite: false,
 });
 
 const geometry = new THREE.BufferGeometry();
 
-export const renderLink = () => new THREE.Line(geometry.clone(), lineMaterial);
+export const renderLink = (link: any) => {
+  const { selectedNode } = useDataStore.getState();
+
+  const selectedLine =
+    selectedNode &&
+    (selectedNode.id === link.source.id || selectedNode.id === link.target.id);
+
+  return new THREE.Line(
+    geometry.clone(),
+    selectedLine ? selectedLineMaterial : lineMaterial
+  );
+};
