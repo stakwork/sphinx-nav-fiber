4ccd75242200c72265813f5911defbbc838fc16c
Bamidele Oluwatobi <teebams49@gmail.com>
2022-09-19 14:24:41 +0100

    feat: save content consumed to relay

--- src/utils/relayHelper/index.ts
+++ src/utils/relayHelper/index.ts
@@ -1,4 +1,5 @@
 import { Node } from "~/types/index";
+import * as sphinx from "sphinx-bridge-kevkevinpal";
 export const getSecondBrainObj = (
   selectedNode: Node | null,
   boost?: number
@@ -20,4 +21,28 @@ export const getSecondBrainObj = (
     type: selectedNode?.details?.type,
     weight: selectedNode?.details?.weight,
   };
+};
+
+export const saveConsumedContent = async (selectedNode: Node | null) => {
+  // @ts-ignore
+  let res: any = await sphinx.enable(true);
+
+  if (!res) {
+    console.log(
+      "Sphinx enable failed, means no pubkey and no budget (including budget of 0)"
+    );
+  }
+
+  try {
+    // @ts-ignore
+    await sphinx.saveGraphData({
+      type: "second_brain_consumed_content",
+      metaData: {
+        date: Math.floor(new Date().getTime() / 1000),
+        ...getSecondBrainObj(selectedNode),
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
