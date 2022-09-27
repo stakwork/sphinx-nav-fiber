38de157dd72b4163e651164e795994b169225182
Bamidele Oluwatobi <teebams49@gmail.com>
2022-09-27 15:51:40 +0100

    update: get lsat from localstorage if it exist

--- src/utils/getLSat/index.ts
+++ src/utils/getLSat/index.ts
@@ -2,32 +2,38 @@ import { Lsat } from "lsat-js";
 import * as sphinx from "sphinx-bridge-kevkevinpal";
 
 export const getLSat = async () => {
-  try {
-    const resp = await fetch("https://knowledge-graph.sphinx.chat/searching");
-    const data = await resp.json();
+  const lsat = localStorage.getItem("lsat");
+  if (!lsat) {
+    try {
+      const resp = await fetch("https://knowledge-graph.sphinx.chat/searching");
+      const data = await resp.json();
 
-    const lsat = Lsat.fromHeader(data.headers);
+      const lsat = Lsat.fromHeader(data.headers);
 
-    // @ts-ignore
-    const LSATRes = await sphinx.saveLsat(
-      lsat.invoice,
-      lsat.baseMacaroon,
-      "knowledge-graph.sphinx.chat"
-    );
-
-    if (LSATRes.success === false) {
       // @ts-ignore
-      await sphinx.topup();
+      const LSATRes = await sphinx.saveLsat(
+        lsat.invoice,
+        lsat.baseMacaroon,
+        "knowledge-graph.sphinx.chat"
+      );
+
+      if (LSATRes.success === false) {
+        // @ts-ignore
+        await sphinx.topup();
+      }
+
+      lsat.setPreimage(LSATRes.lsat.split(":")[1]);
+
+      const token = lsat.toToken();
+      // Need to store Token in Local storage
+      return token;
+    } catch (e) {
+      console.log(e);
+      return null;
     }
-
-    lsat.setPreimage(LSATRes.lsat.split(":")[1]);
-
-    const token = lsat.toToken();
-
-    return token;
-  } catch (e) {
-    console.log(e);
-    return null;
+  } else {
+    // Need to get token from lsat
+    return lsat;
   }
 };
 
