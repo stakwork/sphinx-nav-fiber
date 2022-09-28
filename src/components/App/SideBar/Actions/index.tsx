a82b12f7ff1cc3d68bf5fc0132014246d1b471c8
John Ruddock <john.jxr@pm.me>
2022-09-28 17:11:32 -0400

    feat: adds flagError component to actions

--- src/components/App/SideBar/Actions/index.tsx
+++ src/components/App/SideBar/Actions/index.tsx
@@ -1,13 +1,28 @@
+import styled from "styled-components";
+import { colors } from "~/utils/colors";
 import { Booster } from "~/components/Booster";
 import { Flex } from "~/components/common/Flex";
 import { Pill } from "~/components/common/Pill";
 import { useAppStore } from "~/stores/useAppStore";
 import { useDataStore } from "~/stores/useDataStore";
 
+const FlagError = styled(Flex)`
+  cursor: pointer;
+  padding: 0 0 0 8px;
+  &:hover {
+    color: ${colors.gray200};
+  }
+`;
+
 export const Actions = () => {
   const [transcriptIsOpen, setTranscriptOpen] = useAppStore((s) => [
     s.transcriptIsOpen,
     s.setTranscriptOpen,
+  ]);
+
+  const [flagErrorIsOpen, setFlagErrorOpen] = useAppStore((s) => [
+    s.flagErrorIsOpen,
+    s.setFlagErrorOpen,
   ]);
 
   const selectedNode = useDataStore((s) => s.selectedNode);
@@ -24,6 +39,11 @@ export const Actions = () => {
       <Flex pl={10}>
         <Booster content={selectedNode} refId={selectedNode?.id} />
       </Flex>
+      <FlagError onClick={() => setFlagErrorOpen(!flagErrorIsOpen)}>
+        <span className="material-icons" style={{ fontSize: 30 }}>
+          error_outline
+        </span>
+      </FlagError>
     </Flex>
   );
 };
