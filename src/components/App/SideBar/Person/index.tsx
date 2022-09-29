a24173542766c80a9ce85402f8617ac3ddfe40e5
John Ruddock <john.jxr@pm.me>
2022-09-29 19:18:39 -0400

    fix: sidebar guest view to show image

--- src/components/App/SideBar/Person/index.tsx
+++ src/components/App/SideBar/Person/index.tsx
@@ -1,12 +1,16 @@
 import { Flex } from "~/components/common/Flex";
+import { Avatar } from "~/components/common/Avatar";
 import { useSelectedNode } from "~/stores/useDataStore";
 
 export const Person = () => {
   const selectedNode = useSelectedNode();
 
   return (
-    <Flex align="center" direction="row" justify="center" p={20}>
-      {selectedNode?.label}
+    <Flex direction="row" px={24} py={16}>
+      <Avatar size={45} src="person_placeholder2.png" />
+      <Flex align="center" direction="row" justify="center" p={10}>
+        {selectedNode?.label}
+      </Flex>
     </Flex>
   );
 };
