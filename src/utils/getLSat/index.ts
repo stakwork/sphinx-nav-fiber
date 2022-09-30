594947c13a32c58cafde457ff21386fd81b4c34c
Bamidele Oluwatobi <teebams49@gmail.com>
2022-09-30 03:35:43 +0100

    Get Active Lsat connected with sphinx-bridge and relay

--- src/utils/getLSat/index.ts
+++ src/utils/getLSat/index.ts
@@ -1,13 +1,45 @@
 import { Lsat } from "lsat-js";
 import * as sphinx from "sphinx-bridge-kevkevinpal";
 
+export const getActiveLsat = async () => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await sphinx.enable(true);
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const lsat = await sphinx.getLsat();
+
+    // Save Lsat to local Storage
+    if (lsat.macaroon && lsat.preimage) {
+      localStorage.setItem("lsat", JSON.stringify(lsat));
+      
+      return `LSAT ${lsat.macaroon}:${lsat.preimage}`;
+    } 
+
+      return null;
+    
+  } catch (e) {
+    console.log(e);
+    
+    return null;
+  }
+};
+
 export const getLSat = async () => {
   let lsat = localStorage.getItem("lsat");
   
   if (!lsat) {
-    try {
-      const resp = await fetch("https://knowledge-graph.sphinx.chat/searching");
-      const data = await resp.json();
+    const newLsat = await getActiveLsat();
+
+    if (!newLsat) {
+      try {
+        const resp = await fetch(
+          "https://knowledge-graph.sphinx.chat/searching"
+        );
+        
+        const data = await resp.json();
 
       lsat = Lsat.fromHeader(data.headers);
 
@@ -19,43 +51,36 @@ export const getLSat = async () => {
         "knowledge-graph.sphinx.chat"
       );
 
-      if (LSATRes.success === false) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        await sphinx.topup();
+        if (LSATRes.success === false) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          await sphinx.topup();
+        }
+
+        lsat.setPreimage(LSATRes.lsat.split(":")[1]);
+
+        JSON.stringify({
+          macaroon: lsat.baseMacaroon,
+          identifier: lsat.id,
+          preimage: LSATRes.lsat.split(":")[1],
+        });
+
+        const token = lsat.toToken();
+        // Need to store Token in Local storage
+
+        return token;
+      } catch (e) {
+        console.log(e);
+
+        return null;
       }
-
-      lsat.setPreimage(LSATRes.lsat.split(":")[1]);
-
-      const token = lsat.toToken();
-      // Need to store Token in Local storage
-
-      return token;
-    } catch (e) {
-      console.log(e);
-      
-      return null;
+    } else {
+      return newLsat;
     }
   } else {
     // Need to get token from lsat
-    return lsat;
-  }
-};
+    const newlsat = JSON.parse(lsat);
 
-export const getActiveLsat = async () => {
-  try {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await sphinx.enable(true);
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const lsat = await sphinx.getLsat();
-
-    console.log("Getting right value");
-    // Save Lsat to local Storage
-    console.log(lsat);
-  } catch (e) {
-    console.log(e);
+    return `LSAT ${newlsat.macaroon}:${newlsat.preimage}`;
   }
 };
