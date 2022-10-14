a688c76fcc2e74b9b17d96f3323c6b16328a4ddd
Bamidele Oluwatobi <teebams49@gmail.com>
2022-10-14 19:34:00 +0100

    refactor:  moved the boostAgainstBudget function to new file so its resuable

--- src/utils/boost/index.tsx
+++ src/utils/boost/index.tsx
@@ -1,54 +1,10 @@
-import * as sphinx from "sphinx-bridge-kevkevinpal";
 import { api } from "~/network/api";
-
-const sphinxPubkey =
-  "023d8eb306f0027b902fbdc81d33b49b6558b3434d374626f8c324979c92d47c21";
-
-const boostAgainstBudget = async (amount: number, pubkey: string) => {
-  let err: any = null;
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  let res = await sphinx.enable(true);
-
-  if (!res) {
-    console.log(
-      "Sphinx enable failed, means no pubkey and no budget (including budget of 0)"
-    );
-  }
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  res = await sphinx.keysend(pubkey || sphinxPubkey, amount);
-
-  if (!res || !res.success) {
-    // rejected, ask for topup
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    res = await sphinx.topup();
-
-    if (!res || !res.budget || res?.budget < amount) {
-      // topup failed
-      err = new Error("Topup failed");
-    } else {
-      // retry keysend
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      res = await sphinx.keysend(pubkey || sphinxPubkey, amount);
-
-      if (!res) {
-        err = new Error("Keysend failed after topup");
-      }
-    }
-  }
-
-  return err;
-};
+import { keysendPayment } from "~/utils/keysend";
 
 export const boost = async (refId: string, amount: number, pubkey: string) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const err = await boostAgainstBudget(amount, pubkey);
+  const err = await keysendPayment(amount, pubkey);
 
   if (err) {
     throw new Error(err);
---
+++ src/utils/keysend/index.ts
@@ -0,0 +1,45 @@
+import * as sphinx from "sphinx-bridge-kevkevinpal";
+
+const sphinxPubkey =
+  "023d8eb306f0027b902fbdc81d33b49b6558b3434d374626f8c324979c92d47c21";
+
+export const keysendPayment = async (amount: number, pubkey: string) => {
+  let err: any = null;
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  let res = await sphinx.enable(true);
+
+  if (!res) {
+    console.log(
+      "Sphinx enable failed, means no pubkey and no budget (including budget of 0)"
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  res = await sphinx.keysend(pubkey || sphinxPubkey, amount);
+
+  if (!res) {
+    // rejected, ask for topup
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    res = await sphinx.topup();
+
+    if (!res || !res.budget || res?.budget < amount) {
+      // topup failed
+      err = new Error("Topup failed");
+    } else {
+      // retry keysend
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      res = await sphinx.keysend(pubkey || sphinxPubkey, amount);
+
+      if (!res) {
+        err = new Error("Keysend failed after topup");
+      }
+    }
+  }
+
+  return err;
+};
