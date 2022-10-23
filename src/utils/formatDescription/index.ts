9c04a04a964ffc6bfb72eaab5a8f9ed1c60b1fad
John Ruddock <john.jxr@pm.me>
2022-10-22 20:56:23 -0400

    fix: format description handling

--- src/utils/formatDescription/index.ts
+++ src/utils/formatDescription/index.ts
@@ -1,5 +1,7 @@
-export const formatDescription = (description: string) => {
-  const desc = description.replace(/[[]{2}/g, "").replace(/[\]]{2}/g, "");
-  
-  return desc;
-};
+export function formatDescription(description: string) {
+  if (description == null) {
+    return "Text unavailable";
+  }
+
+  return description.replace(/[[]{2}/g, "").replace(/[\]]{2}/g, "");
+}
