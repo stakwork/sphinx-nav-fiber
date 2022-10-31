d118d9fb77edcedc067b2e916d691d42707aed77
Johnjx <graphur@protonmail.com>
2022-10-31 18:59:34 -0400

    feat: adds budget explanation during enable

--- src/components/App/AppBar/index.tsx
+++ src/components/App/AppBar/index.tsx
@@ -3,6 +3,7 @@ import { Flex } from "~/components/common/Flex";
 import { Text } from "~/components/common/Text";
 import { SearchBar } from "~/components/SearchBar";
 import { Stats } from "~/components/Stats";
+import { colors } from "~/utils/colors";
 import { useAppStore } from "~/stores/useAppStore";
 import { media } from "~/utils/media";
 
@@ -42,7 +43,31 @@ const SearchBarWrapper = styled.div`
   `}
 `;
 
-export const AppBar = () => {
+const BudgetExplainer = styled(Flex).attrs({
+  direction: "row",
+})`
+  width: 25%;
+  position: absolute;
+  top: 220px;
+  left: 25px;
+  border: 1px solid ${colors.primaryBlueBorder};
+  border-radius: 4px;
+  padding: 1%;
+  pointer-events: auto;
+  z-index: 30;
+`;
+
+const CloseButton = styled(Flex)`
+  color: ${colors.mainBottomIcons};
+  cursor: pointer;
+`;
+
+type Props = {
+  isEnabling: boolean;
+  setEnabling: (value: boolean) => void;
+};
+
+export const AppBar = ({ isEnabling, setEnabling }: Props) => {
   const sidebarIsOpen = useAppStore((s) => s.sidebarIsOpen);
 
   if (sidebarIsOpen) {
@@ -63,6 +88,25 @@ export const AppBar = () => {
       <StatsWrapper>
         <Stats />
       </StatsWrapper>
+
+      {isEnabling && (
+        <BudgetExplainer>
+          <Text color="white" kind="regularBold">
+            Each search of the graph costs 10 sats. Please enter a budget to buy
+            an LSAT which will allow you to search up until your budget runs
+            out. You will be able to set a new budget when that happens.
+          </Text>
+          <CloseButton
+            onClick={() => {
+              setEnabling(false);
+            }}
+          >
+            <span className="material-icons" style={{ fontSize: 15 }}>
+              close
+            </span>
+          </CloseButton>
+        </BudgetExplainer>
+      )}
 
       <SearchBarWrapper>
         <SearchBar />
