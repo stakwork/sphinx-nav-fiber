63c994c495ccde77f46dad9be13b62177be266aa
Johnjx <graphur@protonmail.com>
2022-11-09 18:03:02 -0500

    feat: adjust searchbar styling

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -10,12 +10,10 @@ type Props = {
 
 const Input = styled.input<{ loading?: boolean }>`
   pointer-events: auto;
-  border-radius: 100px;
   height: 50px;
   padding: 0 20px;
   z-index: 2;
   box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
-  border-radius: 4px;
   width: 100%;
   color: #fff;
   background-color: rgba(19, 24, 29, 1);
