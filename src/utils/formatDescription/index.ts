b9b8d9dc325e37de0ff30d8227d7c2f0b3f2db69
Johnjx <graphur@protonmail.com>
2022-12-06 13:21:54 -0500

    fix: removes `text unavailable`

--- src/utils/formatDescription/index.ts
+++ src/utils/formatDescription/index.ts
@@ -1,6 +1,6 @@
 export function formatDescription(description: string | undefined) {
   if (!description) {
-    return "Text unavailable";
+    return "";
   }
 
   return description.replace(/[[]{2}/g, "").replace(/[\]]{2}/g, "");
