845539ce584ed415fd7d9de7da5353af6142ab75
John <61718801+Johnjx@users.noreply.github.com>
2022-12-08 13:32:55 -0500

    Fix/node time and messages (#110)
    * feat: adds time to ms func
    
    * fix: removes time to min func
    
    * feat: updates node response messages
    
    * fix: timestamp conversion and error message

---
+++ src/utils/timeToMilliseconds/index.ts
@@ -0,0 +1,7 @@
+export function timeToMilliseconds(time: string) {
+  const [hours, minutes, seconds] = time.split(":").map(Number);
+
+  return Number(
+    (hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000).toFixed(6)
+  );
+}
