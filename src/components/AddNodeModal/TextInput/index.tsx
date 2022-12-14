4ae639b1ea4d6bce9289e66b75f4a69d6fba3e1e
Johnjx <graphur@protonmail.com>
2022-12-14 17:37:53 -0500

    feat: adds question icon and tooltip to textInput comps

--- src/components/AddNodeModal/TextInput/index.tsx
+++ src/components/AddNodeModal/TextInput/index.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-hook-form";
 import styled from "styled-components";
 import { BaseTextInput, BaseTextInputProps } from "~/components/BaseTextInput";
+import { colors } from "~/utils/colors";
 import { Flex } from "~/components/common/Flex";
 import { Text } from "~/components/common/Text";
 
@@ -19,10 +20,11 @@ const Wrapper = styled(Flex).attrs({
 
 type Props = BaseTextInputProps & {
   label: string;
+  message?: string;
   rules?: RegisterOptions;
 };
 
-export const TextInput = ({ label, name, rules, ...props }: Props) => {
+export const TextInput = ({ label, message, name, rules, ...props }: Props) => {
   const {
     control,
     formState: { errors },
@@ -32,10 +34,14 @@ export const TextInput = ({ label, name, rules, ...props }: Props) => {
 
   return (
     <Flex shrink={1}>
-      <Flex pb={4} pl={4}>
+      <Flex align="center" direction="row" pb={4} pl={4}>
         <Text color="lightGray" kind="regularBold">
           {label}
         </Text>
+        <QuestionIcon>
+          <span className="material-icons">question_mark</span>
+          <div className="tooltip">{message}</div>
+        </QuestionIcon>
       </Flex>
       <Wrapper>
         <Controller
@@ -77,3 +83,32 @@ export const TextInput = ({ label, name, rules, ...props }: Props) => {
     </Flex>
   );
 };
+
+const QuestionIcon = styled(Flex).attrs({})`
+  cursor: default;
+  margin-left: 4px;
+  position: relative;
+
+  span {
+    color: ${colors.secondaryText4};
+    font-size: 12px;
+  }
+
+  .tooltip {
+    position: absolute;
+    background-color: ${colors.dashboardHeader};
+    border: 1px solid ${colors.secondaryText4};
+    border-radius: 4px;
+    color: ${colors.white};
+    top: 22px;
+    padding: 4px 8px;
+    font-size: 13px;
+    visibility: hidden;
+    width: 175px;
+    z-index: 1;
+  }
+
+  &:hover .tooltip {
+    visibility: visible;
+  }
+`;
