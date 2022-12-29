4806d3861d61aa330c4afad25e0d9b096ce4d45a
Johnjx <graphur@protonmail.com>
2022-12-29 17:39:02 -0500

    chore: adds default commands, types. adds constants for addNodeForm testing

---
+++ cypress/support/constants/addNode.ts
@@ -0,0 +1,15 @@
+// buttons
+export const addNodeButtonHome = ".kGagkX > .sc-gsnTZi";
+export const addNodeButtonSidebarOpen = ".sc-iIPllB > .sc-gsnTZi > .sc-dkzDqf";
+export const addNodeSubmitButton = ".jkcwwh > .sc-gsnTZi";
+export const addNodeCloseButton = "[data-test=add-node-close-button]";
+
+export const addNodeModal = ".sc-crXcEl";
+export const tagError = ":nth-child(5) > .hSMvhC > .jsuUoS";
+
+// inputs
+export const link = "input[name=link]";
+export const startTime = "input[name=startTime]";
+export const endTime = "input[name=endTime]";
+export const description = "textarea[name=description]";
+export const tags = "input[name=tags]";
