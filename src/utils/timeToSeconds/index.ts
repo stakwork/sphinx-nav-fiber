f057ccde1b8b955160fde2af4a450c6a8d61cea9
John <61718801+Johnjx@users.noreply.github.com>
2023-01-02 13:28:00 -0500

    Fix/add node time ms (#133)
    * fix: remove time to seconds
    
    * fix: add time to milliseconds
    
    * fix: update import and functions

--- src/utils/timeToSeconds/index.ts
+++
@@ -1,5 +0,0 @@
-export function timeToSeconds(time: string) {
-  const [hours, minutes, seconds] = time.split(":").map(Number);
-
-  return Number((hours * 60 * 60 + minutes * 60 + seconds).toFixed(6));
-}
