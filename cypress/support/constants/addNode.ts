6cc5a025224be7ea15b588f5f5836c2ba000f1d2
Johnjx <graphur@protonmail.com>
2023-01-03 15:51:13 -0500

    test(addNodeForm): update constants

--- cypress/support/constants/addNode.ts
+++ cypress/support/constants/addNode.ts
@@ -4,12 +4,14 @@ export const addNodeButtonSidebarOpen = ".sc-iIPllB > .sc-gsnTZi > .sc-dkzDqf";
 export const addNodeSubmitButton = ".jkcwwh > .sc-gsnTZi";
 export const addNodeCloseButton = "[data-test=add-node-close-button]";
 
+// modal & error messages
 export const addNodeModal = ".sc-crXcEl";
 export const tagError = ":nth-child(5) > .hSMvhC > .jsuUoS";
+export const mainError = "[data-test=add-node-error-alert]";
 
 // inputs
 export const link = "input[name=link]";
-export const startTime = "input[name=startTime]";
-export const endTime = "input[name=endTime]";
+export const startTime = ".eKMOQC > .hSMvhC > .sc-gKXOVf > .sc-jSMfEi";
+export const endTime = ".dkSRch > .hSMvhC > .sc-gKXOVf > .sc-jSMfEi";
 export const description = "textarea[name=description]";
 export const tags = "input[name=tags]";
