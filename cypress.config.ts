19da0a5ab3f32dfcedb6574ac24fa048e61ae726
Johnjx <graphur@protonmail.com>
2023-01-04 15:20:54 -0500

    test: add baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: '5pz59y',
   e2e: {
+    baseUrl: 'http://localhost:3000',
     pageLoadTimeout: 90000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
