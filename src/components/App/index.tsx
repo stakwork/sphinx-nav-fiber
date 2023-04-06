76b4f23859abe315125711915bd948d620488547
Rassl <rasul.sahibjanli@gmail.com>
2023-04-06 22:14:09 +0400

    feat: add version, and github action, to update version on merge to master (#213)
    Co-authored-by: Расул <rasul@MacBook-Air.local>

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,89 +1,92 @@
-import { useCallback, useEffect, useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import "react-toastify/dist/ReactToastify.css";
-import * as sphinx from "sphinx-bridge-kevkevinpal";
-import styled from "styled-components";
-import { AddNodeModal } from "~/components/AddNodeModal";
-import { BudgetExplanationModal } from "~/components/BudgetExplanationModal";
-import { Flex } from "~/components/common/Flex";
-import { DataRetriever } from "~/components/DataRetriever";
-import { GlobalStyle } from "~/components/GlobalStyle";
-import { Universe } from "~/components/Universe";
-import { useAppStore } from "~/stores/useAppStore";
-import { useDataStore, useSelectedNode } from "~/stores/useDataStore";
-import { useModal } from "~/stores/useModalStore";
-import { colors } from "~/utils/colors";
-import { Preloader } from "../Universe/Preloader";
-import { AppBar } from "./AppBar";
-import { FooterMenu } from "./FooterMenu";
-import { SecondarySideBar } from "./SecondarySidebar";
-import { SideBar } from "./SideBar";
-import { Toasts } from "./Toasts";
+import { useCallback, useEffect, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import 'react-toastify/dist/ReactToastify.css'
+import * as sphinx from 'sphinx-bridge-kevkevinpal'
+import styled from 'styled-components'
+import { AddNodeModal } from '~/components/AddNodeModal'
+import { BudgetExplanationModal } from '~/components/BudgetExplanationModal'
+import { Flex } from '~/components/common/Flex'
+import { DataRetriever } from '~/components/DataRetriever'
+import { GlobalStyle } from '~/components/GlobalStyle'
+import { Universe } from '~/components/Universe'
+import { useAppStore } from '~/stores/useAppStore'
+import { useDataStore, useSelectedNode } from '~/stores/useDataStore'
+import { useModal } from '~/stores/useModalStore'
+import { colors } from '~/utils/colors'
+import version from '~/utils/versionHelper'
+import { Preloader } from '../Universe/Preloader'
+import { AppBar } from './AppBar'
+import { FooterMenu } from './FooterMenu'
+import { SecondarySideBar } from './SecondarySidebar'
+import { SideBar } from './SideBar'
+import { Toasts } from './Toasts'
 
 const Wrapper = styled(Flex)`
   height: 100%;
   width: 100%;
   background-color: ${colors.black};
+`
+
+const Version = styled(Flex)`
+  position: absolute;
+  bottom: 8px;
+  left: 8px;
+  color: ${colors.white};
+  font-size: 12px;
+  opacity: 0.5;
 `;
 
 export const App = () => {
-  const [isAuthorized, setAuthorized] = useState(false);
+  const [isAuthorized, setAuthorized] = useState(false)
 
-  const { open } = useModal("budgetExplanation");
+  const { open } = useModal('budgetExplanation')
 
-  const selectedNode = useSelectedNode();
-  const setSidebarOpen = useAppStore((s) => s.setSidebarOpen);
+  const selectedNode = useSelectedNode()
+  const setSidebarOpen = useAppStore((s) => s.setSidebarOpen)
 
-  const [
-    searchTerm,
-    setCurrentSearch,
-    setRelevanceSelected,
-    setTranscriptOpen,
-  ] = useAppStore((s) => [
+  const [searchTerm, setCurrentSearch, setRelevanceSelected, setTranscriptOpen] = useAppStore((s) => [
     s.currentSearch,
     s.setCurrentSearch,
     s.setRelevanceSelected,
     s.setTranscriptOpen,
-  ]);
+  ])
 
-  const hasBudgetExplanationModalBeSeen = useAppStore(
-    (s) => s.hasBudgetExplanationModalBeSeen
-  );
+  const hasBudgetExplanationModalBeSeen = useAppStore((s) => s.hasBudgetExplanationModalBeSeen)
 
-  const fetchData = useDataStore((s) => s.fetchData);
-  const setSphinxModalOpen = useDataStore((s) => s.setSphinxModalOpen);
-  const setSelectedNode = useDataStore((s) => s.setSelectedNode);
-  const setCategoryFilter = useDataStore((s) => s.setCategoryFilter);
+  const fetchData = useDataStore((s) => s.fetchData)
+  const setSphinxModalOpen = useDataStore((s) => s.setSphinxModalOpen)
+  const setSelectedNode = useDataStore((s) => s.setSelectedNode)
+  const setCategoryFilter = useDataStore((s) => s.setCategoryFilter)
 
-  const form = useForm<{ search: string }>({ mode: "onChange" });
+  const form = useForm<{ search: string }>({ mode: 'onChange' })
 
   const handleSubmit = form.handleSubmit(({ search }) => {
-    setTranscriptOpen(false);
-    setSelectedNode(null);
-    setRelevanceSelected(false);
-    setCurrentSearch(search);
-    setCategoryFilter(null);
-  });
+    setTranscriptOpen(false)
+    setSelectedNode(null)
+    setRelevanceSelected(false)
+    setCurrentSearch(search)
+    setCategoryFilter(null)
+  })
 
-  const showSideBar = !!selectedNode || isAuthorized;
+  const showSideBar = !!selectedNode || isAuthorized
 
   useEffect(() => {
-    setSidebarOpen(showSideBar);
-  }, [isAuthorized, selectedNode, setSidebarOpen, showSideBar]);
+    setSidebarOpen(showSideBar)
+  }, [isAuthorized, selectedNode, setSidebarOpen, showSideBar])
 
   const runSearch = useCallback(async () => {
     if (searchTerm) {
-      setSphinxModalOpen(true);
+      setSphinxModalOpen(true)
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      await sphinx.enable();
-      setSphinxModalOpen(false);
+      await sphinx.enable()
+      setSphinxModalOpen(false)
 
-      setAuthorized(true);
+      setAuthorized(true)
     }
 
-    fetchData(searchTerm);
-  }, [fetchData, searchTerm, setSphinxModalOpen]);
+    fetchData(searchTerm)
+  }, [fetchData, searchTerm, setSphinxModalOpen])
 
   useEffect(() => {
     if (searchTerm) {
@@ -94,9 +97,9 @@ export const App = () => {
       }
     }
 
-    runSearch();
+    runSearch()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchTerm, runSearch, hasBudgetExplanationModalBeSeen]);
+  }, [searchTerm, runSearch, hasBudgetExplanationModalBeSeen])
 
   return (
     <>
@@ -114,6 +117,7 @@ export const App = () => {
             <AppBar onSubmit={handleSubmit} />
 
             <FooterMenu />
+            <Version>v{version}</Version>
           </FormProvider>
         </DataRetriever>
 
@@ -124,5 +128,5 @@ export const App = () => {
         <BudgetExplanationModal />
       </Wrapper>
     </>
-  );
-};
+  )
+}
