27140b6f2d04629577137f49a7fdf4e99871d7f6
Расул <rasul@MacBook-Air.local>
2023-04-16 01:29:25 +0300

    feat: fix table scroll, remove start value from sentiment chart

--- src/components/App/SecondarySidebar/SourcesView/index.tsx
+++ src/components/App/SecondarySidebar/SourcesView/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
-import Typography from '@mui/material/Typography'
 import { Sources } from './Sources'
 import { colors } from '~/utils/colors'
 import { QueuedSources } from './QueuedSources'
 import styled from 'styled-components'
+import { Flex } from '~/components/common/Flex'
 
 interface TabPanelProps {
   children?: React.ReactNode
@@ -17,19 +17,17 @@ const TabPanel = (props: TabPanelProps) => {
   const { children, value, index, ...other } = props
 
   return (
-    <div
-      aria-labelledby={`simple-tab-${index}`}
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      role="tabpanel"
-      {...other}
-    >
-      {value === index && (
-        <div>
-          <Typography>{children}</Typography>
-        </div>
-      )}
-    </div>
+    value === index ? (
+      <TabPanelWrapper
+        aria-labelledby={`simple-tab-${index}`}
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        role="tabpanel"
+        {...other}
+      >
+        {children}
+      </TabPanelWrapper>
+    ) : null
   )
 }
 
@@ -48,20 +46,18 @@ export const SourcesView = () => {
   }
 
   return (
-    <div>
-      <div>
-        <Tabs aria-label="basic tabs example" onChange={handleChange} value={value}>
-          <StyledTab label="Sources table" {...a11yProps(0)} />
-          <StyledTab color={colors.white} label="Queued sources" {...a11yProps(1)} />
-        </Tabs>
-      </div>
+    <Wrapper direction="column">
+      <Tabs aria-label="basic tabs example" onChange={handleChange} value={value}>
+        <StyledTab label="Sources table" {...a11yProps(0)} />
+        <StyledTab color={colors.white} label="Queued sources" {...a11yProps(1)} />
+      </Tabs>
       <TabPanel index={0} value={value}>
         <Sources />
       </TabPanel>
       <TabPanel index={1} value={value}>
         <QueuedSources />
       </TabPanel>
-    </div>
+    </Wrapper>
   )
 }
 
@@ -69,4 +65,15 @@ const StyledTab = styled(Tab)`
   && {
     color: ${colors.white};
   }
+`
+
+const TabPanelWrapper = styled(Flex)`
+  display: flex;
+  flex: 1;
+  min-height: 0;
+`
+
+const Wrapper = styled(Flex)`
+  min-height: 0;
+  flex: 1;
 `
