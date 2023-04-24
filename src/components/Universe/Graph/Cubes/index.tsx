5a9e3c3b4f24f4180cdc146da7c0fa30ab8ad003
David Plell <plelldavid@gmail.com>
2023-04-24 11:27:07 -0700

    revert changes in cubes

--- src/components/Universe/Graph/Cubes/index.tsx
+++ src/components/Universe/Graph/Cubes/index.tsx
@@ -1,12 +1,11 @@
 import { Select } from "@react-three/drei";
-import { memo, useCallback, useEffect } from "react";
+import { memo, useCallback } from "react";
 import { Object3D } from "three";
 import { useGraphData } from "~/components/DataRetriever";
 import { useAppStore } from "~/stores/useAppStore";
 import { useDataStore } from "~/stores/useDataStore";
 import { NodeExtended } from "~/types";
 import { Cube } from "./Cube";
-import { playInspectSound } from '../../../common/Sounds/index'
 
 type NodeTypeColors = Record<string, string>;
 
@@ -22,10 +21,6 @@ export const Cubes = memo(() => {
 
   const handleSelect = useCallback((nodes: Object3D[]) => {
     const node = nodes?.[0];
-
-    // if (node) {
-    //   playInspectSound(node?.userData?.node_type||'')
-    // }
 
     useDataStore
       .getState()
