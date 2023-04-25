a2a21c17b03429d0f99e1c748bf4f09510d039e1
Rassl <rasul.sahibjanli@gmail.com>
2023-04-25 15:21:26 +0400

    Feature/227 (#248)
    * feat: add post, get to about page
    
    * feat: seperate common view and admin view
    
    ---------
    
    Co-authored-by: Расул <rasul@MacBook-Air.local>

---
+++ src/components/App/SecondarySidebar/About/CommonView/index.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import { Flex } from "~/components/common/Flex";
+import { Text } from "~/components/common/Text";
+import { TAboutParams } from "~/network/fetchSourcesData";
+
+type Props = {
+  initialValues: TAboutParams
+}
+
+export const CommonView: FC<Props> = ({ initialValues }) => (
+  <>
+    {initialValues.title && (
+      <Flex pt={12}>
+        <Text kind="regular">Graph Title</Text>
+        <Flex pt={12}>
+          <Text className="value" kind="medium">
+            {initialValues.title}
+          </Text>
+        </Flex>
+      </Flex>
+    )}
+    {initialValues.description && (
+      <Flex pt={12}>
+        <Text kind="regular">Graph Description</Text>
+        <Flex pt={12}>
+          <Text className="value" kind="medium">
+            {initialValues.description}
+          </Text>
+        </Flex>
+      </Flex>
+    )}
+    {initialValues.mission_statement && (
+    <Flex pt={12}>
+      <Text kind="regular">Mission Statement</Text>
+      <Flex pt={12}>
+        <Text className="value" kind="medium">
+          {initialValues.mission_statement}
+        </Text>
+      </Flex>
+    </Flex>
+    )}
+  </>
+);
