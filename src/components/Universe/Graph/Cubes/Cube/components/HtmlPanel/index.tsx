944509e952487e5f446198a0052667940a61b5a0
Emberist <13602797+emberist@users.noreply.github.com>
2023-04-26 12:28:03 +0200

    fix: component  HtmlPanel types

--- src/components/Universe/Graph/Cubes/Cube/components/HtmlPanel/index.tsx
+++ src/components/Universe/Graph/Cubes/Cube/components/HtmlPanel/index.tsx
@@ -1,61 +1,65 @@
-import { Html, Float } from "@react-three/drei";
-import styled, { CSSProperties } from "styled-components";
+import { Float, Html } from '@react-three/drei'
+import styled, { css } from 'styled-components'
 
 type Props = {
-    children: React.ReactNode;
-    speed?: number;
-    intensity?: number;
-    style?: Record<string, unknown>;
-};
+  children: React.ReactNode
+  dimensions?: [number, number, number, number]
+  speed?: number
+  intensity?: number
+}
 
-export const HtmlPanel = ({ speed = 2, intensity = 4, style, children }: Props) => (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    <Float
+export const HtmlPanel = ({ dimensions, speed = 2, intensity = 4, children }: Props) => (
+  <Float
+    attach="geometry"
     floatingRange={[1, 2]}
-    floatIntensity={intensity} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-    speed={speed} // Animation speed, defaults to 1
-    ><Html
-    center
-    onClick={e => e.stopPropagation()}
-    onKeyDown={e=>e.stopPropagation()}
-    onPointerDown={e => e.stopPropagation()}
-    onPointerOut={e=>e.stopPropagation()}
-    onPointerOver={e=>e.stopPropagation()}
-    onPointerUp={e=>e.stopPropagation()}
-    scale={6}
-    sprite
-    transform
+    /* Up/down float intensity, works like a multiplier with floatingRange,defaults to 1 */
+    floatIntensity={intensity}
+    /* Animation speed, defaults to 1 */
+    speed={speed}
+  >
+    <Html
+      center
+      onClick={(e) => e.stopPropagation()}
+      onKeyDown={(e) => e.stopPropagation()}
+      onPointerDown={(e) => e.stopPropagation()}
+      onPointerOut={(e) => e.stopPropagation()}
+      onPointerOver={(e) => e.stopPropagation()}
+      onPointerUp={(e) => e.stopPropagation()}
+      scale={6}
+      sprite
+      transform
     >
-            <HtmlWrap
-                onPointerDown={e => e.stopPropagation()}
-                onPointerOut={e => e.stopPropagation()}
-                onPointerOver={e => e.stopPropagation()}
-                onPointerUp={e => e.stopPropagation()}
-                style={style}
-                >
-                    {children}
-            </HtmlWrap>
-      </Html>
-    </Float>
+      <HtmlWrap
+        dimensions={dimensions}
+        onPointerDown={(e) => e.stopPropagation()}
+        onPointerOut={(e) => e.stopPropagation()}
+        onPointerOver={(e) => e.stopPropagation()}
+        onPointerUp={(e) => e.stopPropagation()}
+      >
+        {children}
+      </HtmlWrap>
+    </Html>
+  </Float>
 )
 
-type HtmlWrapProps = {
-    style: (CSSProperties & Record<string, unknown>) | undefined;
-}
-    
-const HtmlWrap = styled.div<HtmlWrapProps>`
-position: absolute;
-top: ${p =>`${p.style?.top||-200}px`};
-left: ${p =>`${p.style?.left||-680}px`};
-border-radius: 20px;
-// box-shadow:-8px -6px #5078f2;
-width: ${p => `${p.style?.width||500}px`};
-height: ${p => `${p.style?.height||500}px`};
-font-size: 80px; 
-color: #fff;
-padding: 20px; 
-border: solid 8px #5078f2;
-background: #00000099;
-overflow-y: auto;
+const defaultDimensions = [-200, -680, 500, 500] as const
+
+const HtmlWrap = styled.div<Pick<Props, 'dimensions'>>`
+  ${({ dimensions: [top, left, width, height] = defaultDimensions }) =>
+    css`
+      top: ${top}px;
+      left: ${left}px;
+      width: ${width}px;
+      height: ${height}px;
+    `}
+
+  position: absolute;
+  border-radius: 20px;
+
+  font-size: 80px;
+  color: #fff;
+  padding: 20px;
+  border: solid 8px #5078f2;
+  background: #00000099;
+  overflow-y: auto;
 `
--- src/components/Universe/Graph/Cubes/Cube/index.tsx
+++ src/components/Universe/Graph/Cubes/Cube/index.tsx
@@ -1,85 +1,79 @@
-import { useFrame, ThreeEvent} from "@react-three/fiber";
-import { Select } from "@react-three/postprocessing";
-import { memo, useCallback, useEffect, useRef, useState } from "react";
-import * as THREE from "three";
-import { usePathway } from "~/components/DataRetriever";
-import { useDataStore, useSelectedNode } from "~/stores/useDataStore";
-import { useAppStore } from "~/stores/useAppStore";
-import { NodeExtended } from "~/types";
-import { PathwayBadge } from "./components/PathwayBadge";
-import { Portal } from "./components/Portal";
-import { Tooltip } from "./components/Tooltip";
-import { useMaterial } from "./hooks/useMaterial";
-import { View } from '../../../../App/SideBar/View/index'
-import { Transcript } from "~/components/App/SideBar/Transcript";
-import { HtmlPanel } from "./components/HtmlPanel"
+import { ThreeEvent, useFrame } from '@react-three/fiber'
+import { Select } from '@react-three/postprocessing'
+import { memo, useCallback, useEffect, useRef, useState } from 'react'
+import * as THREE from 'three'
+import { Transcript } from '~/components/App/SideBar/Transcript'
+import { View } from '~/components/App/SideBar/View'
+import { usePathway } from '~/components/DataRetriever'
+import { useAppStore } from '~/stores/useAppStore'
+import { useDataStore, useSelectedNode } from '~/stores/useDataStore'
+import { NodeExtended } from '~/types'
+import { HtmlPanel } from './components/HtmlPanel'
+import { PathwayBadge } from './components/PathwayBadge'
+import { Portal } from './components/Portal'
+import { Tooltip } from './components/Tooltip'
+import { useMaterial } from './hooks/useMaterial'
 
-
-const geometryXs = new THREE.BoxGeometry(10, 10, 10);
-const geometryS = new THREE.BoxGeometry(20, 20, 20);
-const geometryM = new THREE.BoxGeometry(35, 35, 35);
+const geometryXs = new THREE.BoxGeometry(10, 10, 10)
+const geometryS = new THREE.BoxGeometry(20, 20, 20)
+const geometryM = new THREE.BoxGeometry(35, 35, 35)
 
 const getGeometry = (node: NodeExtended) => {
   switch (node.node_type) {
-    case "guest":
-    case "episode":
-      return geometryS;
-    case "show":
-      return geometryM;
+    case 'guest':
+    case 'episode':
+      return geometryS
+    case 'show':
+      return geometryM
     default:
-      return geometryXs;
+      return geometryXs
   }
-};
+}
 
 type Props = {
-  node: NodeExtended;
-  highlight: boolean;
-  highlightColor: string;
-};
+  node: NodeExtended
+  highlight: boolean
+  highlightColor: string
+}
 
 export const Cube = memo(({ node, highlight, highlightColor }: Props) => {
-  const ref = useRef<THREE.Mesh | null>(null);
-  const [hovered, setHovered] = useState(false);
+  const ref = useRef<THREE.Mesh | null>(null)
+  const [hovered, setHovered] = useState(false)
 
-  const categoryFilter = useDataStore((s) => s.categoryFilter);
+  const categoryFilter = useDataStore((s) => s.categoryFilter)
 
   const transcriptIsOpen = useAppStore((s) => s.transcriptIsOpen)
 
-  const selectedNode = useSelectedNode();
-  
+  const selectedNode = useSelectedNode()
 
-  const material = useMaterial(
-    node.image_url || "noimage.jpeg",
-    highlight,
-    highlightColor
-  );
+  const material = useMaterial(node.image_url || 'noimage.jpeg', highlight, highlightColor)
 
-  const isSelected = selectedNode?.id === node.id;
-  const isSelectedCategory = node.node_type === categoryFilter;
+  const isSelected = selectedNode?.id === node.id
+  const isSelectedCategory = node.node_type === categoryFilter
 
   useFrame(() => {
     if (selectedNode) {
-      material.toneMapped = false;
+      material.toneMapped = false
     }
 
-    ref.current?.position.set(node.x || 0, node.y || 0, node.z || 0);
-  });
+    ref.current?.position.set(node.x || 0, node.y || 0, node.z || 0)
+  })
 
   useEffect(() => {
-    document.body.style.cursor = hovered ? "pointer" : "auto";
-  }, [hovered]);
+    document.body.style.cursor = hovered ? 'pointer' : 'auto'
+  }, [hovered])
 
   const onPointerIn = useCallback((e: ThreeEvent<PointerEvent>) => {
-    e.stopPropagation();
+    e.stopPropagation()
     setHovered(true)
-  }, []);
+  }, [])
 
   const onPointerOut = useCallback((e: ThreeEvent<PointerEvent>) => {
-    e.stopPropagation();
-    setHovered(false);
-  }, []);
+    e.stopPropagation()
+    setHovered(false)
+  }, [])
 
-  const { currentNodeIndex } = usePathway();
+  const { currentNodeIndex } = usePathway()
 
   return (
     <>
@@ -91,31 +85,22 @@ export const Cube = memo(({ node, highlight, highlightColor }: Props) => {
           name={node.id}
           onPointerOut={onPointerOut}
           onPointerOver={onPointerIn}
-          scale={(hovered && !isSelected) ? 1.5 : 1}
+          scale={hovered && !isSelected ? 1.5 : 1}
           userData={node}
         >
-
-          {isSelected &&
+          {isSelected && (
             <HtmlPanel>
-            <View isSelectedView/>
+              <View isSelectedView />
             </HtmlPanel>
-          }
-     
-          
-          {isSelected && transcriptIsOpen &&
-            <HtmlPanel
-            intensity={2}
-            speed={4}
-            style={{ top: 260, left: -40,  width: 600, height: 300}}
-            >
+          )}
+
+          {isSelected && transcriptIsOpen && (
+            <HtmlPanel dimensions={[260, -40, 600, 300]} intensity={2} speed={4}>
               <Transcript />
             </HtmlPanel>
-            }
-            
-          <PathwayBadge
-            show={currentNodeIndex >= 0}
-            value={currentNodeIndex + 1}
-          />
+          )}
+
+          <PathwayBadge show={currentNodeIndex >= 0} value={currentNodeIndex + 1} />
 
           {hovered && (
             <Portal>
@@ -125,7 +110,7 @@ export const Cube = memo(({ node, highlight, highlightColor }: Props) => {
         </mesh>
       </Select>
     </>
-  );
-});
+  )
+})
 
-Cube.displayName = "Cube";
+Cube.displayName = 'Cube'
