02c15b9205cc60cf49ddafef52acc0071ce71f9b
Расул <rasul@MacBook-Air.local>
2023-04-28 16:33:30 +0300

    feat: add webpage content type

---
+++ src/components/AddNodeModal/WebPage/index.tsx
@@ -0,0 +1,29 @@
+import { Flex } from '~/components/common/Flex'
+import { requiredRule } from '../index'
+import { TextInput } from '../TextInput'
+
+const message = `Submit a valid webpage url. If you find a webpage with great text content you’d like to add to the graph, use this option (e.g. A news article, a wiki page, etc). This is NOT for audio or video content.`;
+
+const urlRegex =
+  /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*\.[a-zA-Z]{2,10}(?:\/[^\s]*)?$/
+
+const WebPage = () => (
+  <Flex>
+    <TextInput
+      id="tweet-id"
+      label="Web page url"
+      message={message}
+      name="web_page"
+      placeholder="Paste your url here..."
+      rules={{
+        ...requiredRule,
+        pattern: {
+          message: 'You must enter a valid webpage url',
+          value: urlRegex,
+        },
+      }}
+    />
+  </Flex>
+)
+
+export default WebPage
