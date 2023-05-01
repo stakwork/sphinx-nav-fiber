1c8b33c5683349e95663c6946721a9c3a353a802
Расул <rasul@MacBook-Air.local>
2023-05-02 00:56:35 +0300

    feat: add tribe_message nodes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,23 @@
 /* eslint-disable camelcase */
-import { Mesh } from "three";
+import { Mesh } from 'three'
 
 export type FetchDataResponse = {
-  data_series?: { title: string };
-  exact: Node[];
-  related: Node[];
-};
+  data_series?: { title: string }
+  exact: Node[]
+  related: Node[]
+}
 
 export type FetchSentimentResponse = {
-  data: Sentiment[];
-};
+  data: Sentiment[]
+}
 
 export type FetchRadarResponse = {
   data: Sources[]
 }
 
 export type RadarRequest = {
-  source: string,
-  source_type: string,
+  source: string
+  source_type: string
 }
 
 export type Node = {
@@ -30,6 +30,9 @@ export type Node = {
   guests?: (null | string | Guests)[]
   id?: string
   image_url?: string
+  sender_pic?: string
+  sender_alias?: string
+  message_content?: string
   keyword?: boolean
   label: string
   link?: string
@@ -48,74 +51,67 @@ export type Node = {
 }
 
 export type DataSeriesNode = {
-  id: string;
-  image_url?: string;
-  keyword?: boolean;
-  label: string;
-  link?: string;
-  name: string;
-  node_type: string;
-  ref_id: string;
-  scale?: number;
-  show_title?: string;
-  text?: string;
-  timestamp?: string;
-  topics?: string[];
-  type?: string;
-  weight?: number;
-};
+  id: string
+  image_url?: string
+  keyword?: boolean
+  label: string
+  link?: string
+  name: string
+  node_type: string
+  ref_id: string
+  scale?: number
+  show_title?: string
+  text?: string
+  timestamp?: string
+  topics?: string[]
+  type?: string
+  weight?: number
+}
 
 export type NodeExtended = Node & {
-  x?: number;
-  y?: number;
-  z?: number;
-};
+  x?: number
+  y?: number
+  z?: number
+}
 
 export type Link<T = string> = {
-  index?: T extends string ? never : number;
-  source: T;
-  target: T;
-};
+  index?: T extends string ? never : number
+  source: T
+  target: T
+}
 
 export type GraphData<T = string> = {
-  links: Link<T>[];
-  nodes: NodeExtended[];
-};
+  links: Link<T>[]
+  nodes: NodeExtended[]
+}
 
 export class NodeMesh extends Mesh {
-  __data?: NodeExtended;
+  __data?: NodeExtended
 }
 
 export type Guests = {
-  name: string;
-  ref_id: string;
-  profile_picture: string;
-  twitter_handle: string;
-};
+  name: string
+  ref_id: string
+  profile_picture: string
+  twitter_handle: string
+}
 
-export const nodeTypes = ["clip", "episode", "guest", "show", "topic", "data_series", "tweet"] as const;
+export const nodeTypes = ['clip', 'episode', 'guest', 'show', 'topic', 'data_series', 'tweet'] as const
 
-export type NodeType = typeof nodeTypes[number];
+export type NodeType = (typeof nodeTypes)[number]
 
-export type Type =
-  | "twitter"
-  | "youtube"
-  | "guest"
-  | "podcast"
-  | "tweet"
-  | "data_series"
-  | "twitter_space";
+export type Type = 'twitter' | 'youtube' | 'guest' | 'podcast' | 'tweet' | 'data_series' | 'twitter_space'
 
 export type Sentiment = {
-  date: number;
-  date_added_to_graph: number;
-  sentiment_score: number;
-};
+  date: number
+  date_added_to_graph: number
+  sentiment_score: number
+}
 
 export type Sources = {
-  source_type: string,
-  source: string,
-  id: string,
+  source_type: string
+  source: string
+  id: string
 }
 
 export type SubmitErrRes = {
