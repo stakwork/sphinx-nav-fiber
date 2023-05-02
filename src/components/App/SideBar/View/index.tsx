8ce3fd863b97c6d62abab4797c2365e97cfd2a07
David Plell <plelldavid@gmail.com>
2023-05-02 09:28:48 -0700

    feat: mark

--- src/components/App/SideBar/View/index.tsx
+++ src/components/App/SideBar/View/index.tsx
@@ -1,33 +1,40 @@
-import { useSelectedNode } from "~/stores/useDataStore";
-import { Creator } from "../Creator";
-import { Data } from "../Data";
-import { Person } from "../Person";
-import { Relevance } from "../Relevance";
-import { TwitData } from "../TwitData";
-import { Twitter } from "../Twitter";
+import { useSelectedNode } from '~/stores/useDataStore'
+import { AudioClip } from '../AudioClip'
+import { Creator } from '../Creator'
+import { Data } from '../Data'
+import { Person } from '../Person'
+import { Relevance } from '../Relevance'
+import { TwitData } from '../TwitData'
+import { Twitter } from '../Twitter'
 
 type Props = {
   isSelectedView?: boolean
 }
 
-export const View = ({isSelectedView}: Props) => {
-  const selectedNode = useSelectedNode();
-  
+export const View = ({ isSelectedView }: Props) => {
+  const selectedNode = useSelectedNode()
+
+  if (selectedNode) {
+    console.log(selectedNode.type)
+    console.log(selectedNode.node_type)
+  }
+
   if (isSelectedView) {
-    switch (selectedNode?.type) {
-      case "twitter":
-        return <Twitter />;
-      case "guest":
-        return <Person />;
-      case "data_series":
-        return <Data />;
-      case "tweet":
-        return <TwitData />;
+    switch (selectedNode?.node_type) {
+      case 'twitter':
+        return <Twitter />
+      case 'guest':
+        return <Person />
+      case 'data_series':
+        return <Data />
+      case 'tweet':
+        return <TwitData />
+      case 'clip':
+        return <AudioClip />
       default:
-        return <Creator />;
+        return <Creator />
     }
+  } else {
+    return <Relevance />
   }
-  else {
-    return <Relevance />;
-  }
-};
+}
