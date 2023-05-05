384a1991eea66cf189fb62219b68a1e24db6e8eb
maradondt <maradondt@gmail.com>
2023-05-05 19:25:09 +0200

    test: add test for sentiment chart

--- cypress/support/constants/common.ts
+++ cypress/support/constants/common.ts
@@ -1,11 +1,13 @@
 export const host = 'knowledge-graph.sphinx.chat'
 
-export const actionMenu = '#cy-actions-menu-toggle'
-export const addNodeSubmitButton = '#add-node-submit-cta'
 export const sidebarWrapper = '#sidebar-wrapper'
 export const loader = '#loader'
 
+// buttons
 export const addNodeModal = '#add-node-form'
+export const closeSidebar = '#cy-close-secondary-sidebar'
+export const actionMenu = '#cy-actions-menu-toggle'
+export const addNodeSubmitButton = '#add-node-submit-cta'
 
 // alerts
 export const toast = '.Toastify__toast-body'
