40c1d664d90b5fd34511947db9178928941ebb3b
David Plell <plelldavid@gmail.com>
2023-05-24 10:33:44 -0700

    fix: rm select cube component

--- src/components/Universe/Graph/Cubes/Cube/index.tsx
+++ src/components/Universe/Graph/Cubes/Cube/index.tsx
@@ -1,7 +1,5 @@
-import { Select } from '@react-three/postprocessing'
 import { memo, useRef } from 'react'
 import { Mesh } from 'three'
-import { useSelectedNode } from '~/stores/useDataStore'
 import { NodeExtended } from '~/types'
 import { boxGeometry } from '../constants'
 import { useMaterial } from './hooks/useMaterial'
@@ -13,22 +11,17 @@ export const Cube = memo(({ node }: Props) => {
   const ref = useRef<Mesh | null>(null)
 
   const material = useMaterial(node.image_url || 'noimage.jpeg')
-  const selectedNode = useSelectedNode()
-
-  const isSelected = selectedNode?.ref_id === node?.ref_id
 
   return (
-    <Select enabled={isSelected}>
-      <mesh
-        ref={ref}
-        geometry={boxGeometry}
-        material={material}
-        name={node.id}
-        position={[node.x, node.y, node.z]}
-        scale={node.scale}
-        userData={node}
-      />
-    </Select>
+    <mesh
+      ref={ref}
+      geometry={boxGeometry}
+      material={material}
+      name={node.id}
+      position={[node.x, node.y, node.z]}
+      scale={node.scale}
+      userData={node}
+    />
   )
 })
 
