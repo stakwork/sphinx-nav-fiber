37a6528c800d04120101a118e4e22dd6673c4640
kevkevin <oapallikunnel@gmail.com>
2023-05-30 16:51:53 -0500

    fix: docker build was failing this fixes the error (#314)

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,24 @@
-const CracoAlias = require("craco-alias");
+const CracoAlias = require('craco-alias')
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      })
+      return webpackConfig
+    },
+  },
   plugins: [
     {
       plugin: CracoAlias,
       options: {
         // see in examples section
-        source: "tsconfig",
-        tsConfigPath: "./tsconfig.paths.json",
+        source: 'tsconfig',
+        tsConfigPath: './tsconfig.paths.json',
       },
     },
   ],
-};
+}
