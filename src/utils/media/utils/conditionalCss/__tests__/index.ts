416cbed27ac3b55530ab2c717b58e4027b691974
VladimirZhigalev <maradondt@gmail.com>
2023-06-17 00:22:08 +0200

    feat: add setup for jest (#323)
    * feat: add setup for jest
    
    * fix: fix failde tests & add tests to the actions
    
    * fix: actions

--- src/utils/media/utils/conditionalCss/__tests__/index.ts
+++ src/utils/media/utils/conditionalCss/__tests__/index.ts
@@ -4,17 +4,9 @@ describe('conditionalCss', () => {
   it('returns the speficied styles when the predicate is true', () => {
     const predicate = (windowWidth: number) => windowWidth >= 1000
 
-    const conditionalPadding = conditionalCss(predicate)`
-      padding: 16px;
-    `
+    const conditionalPadding = conditionalCss(predicate)`padding: 16px;`
 
-    expect(conditionalPadding({ windowWidth: 1000 })).toMatchInlineSnapshot(`
-      Array [
-        "
-            padding: 16px;
-          ",
-      ]
-    `)
+    expect(JSON.stringify(conditionalPadding({ windowWidth: 1000 }))).toEqual(JSON.stringify(['padding: 16px;']))
 
     expect(conditionalPadding({ windowWidth: 999 })).toBe(false)
   })
