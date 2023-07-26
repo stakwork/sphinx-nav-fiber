75bfbaeaa733bd812919beb6e4363628a4028afa
Emberist <emberistemac@proton.me>
2023-07-26 22:17:55 +0200

    fix: increment instances limit

--- src/components/Universe/Graph/Cubes/BlurryInstances/constants.ts
+++ src/components/Universe/Graph/Cubes/BlurryInstances/constants.ts
@@ -4,3 +4,5 @@ import { smoothness } from '../Cube/constants'
 const materialOptions = { ...smoothness }
 
 export const blurryMaterial = new MeshStandardMaterial(materialOptions)
+
+export const maxInstances = 2000
--- src/components/Universe/Graph/Cubes/BlurryInstances/index.tsx
+++ src/components/Universe/Graph/Cubes/BlurryInstances/index.tsx
@@ -2,9 +2,9 @@ import { Instance, Instances } from '@react-three/drei'
 import { useMemo } from 'react'
 import { useGraphData } from '~/components/DataRetriever'
 import { getNodeColorByType } from '~/components/Universe/Graph/constant'
-import { useDataStore } from '~/stores/useDataStore'
+import { useDataStore, useSelectedNode } from '~/stores/useDataStore'
 import { boxGeometry, isMainTopic } from '../constants'
-import { blurryMaterial } from './constants'
+import { blurryMaterial, maxInstances } from './constants'
 
 type InstanceProps = {
   hide?: boolean
@@ -12,33 +12,42 @@ type InstanceProps = {
 
 export const BlurryInstances = ({ hide }: InstanceProps) => {
   const data = useGraphData()
+  const selectedNode = useSelectedNode()
 
   const graphStyle = useDataStore((s) => s.graphStyle)
 
+  const nearbyNodeIds = useDataStore((s) => s.nearbyNodeIds)
+
   const instances = useMemo(
     () =>
-      data.nodes.map((node, i) => {
-        const visible = !isMainTopic(node)
-        const color = getNodeColorByType(node.node_type || '', true) as string
+      data.nodes
+        .filter((f) => {
+          const isNotSelected = f.ref_id !== selectedNode?.ref_id
+          const isNearbyOrPersistent = nearbyNodeIds.includes(f.ref_id || '')
 
-        return (
-          <Instance
-            // eslint-disable-next-line react/no-array-index-key
-            key={`${node.ref_id || node.id}-instanced-node-${i}-${graphStyle}`}
-            color={color}
-            name={node.id}
-            position={[node.x, node.y, node.z]}
-            scale={visible ? (node.scale || 1) * 0.9 : 0}
-            userData={node}
-          />
-        )
-      }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [graphStyle, data],
+          return isNotSelected && !isNearbyOrPersistent
+        })
+        .map((node, i) => {
+          const visible = !isMainTopic(node)
+          const color = getNodeColorByType(node.node_type || '', true) as string
+
+          return (
+            <Instance
+              // eslint-disable-next-line react/no-array-index-key
+              key={`${node.ref_id || node.id}-instanced-node-${i}-${graphStyle}`}
+              color={color}
+              name={node.id}
+              position={[node.x, node.y, node.z]}
+              scale={visible ? (node.scale || 1) * 0.9 : 0}
+              userData={node}
+            />
+          )
+        }),
+    [data.nodes, graphStyle, nearbyNodeIds, selectedNode],
   )
 
   return (
-    <Instances geometry={boxGeometry} material={blurryMaterial} visible={!hide}>
+    <Instances geometry={boxGeometry} limit={maxInstances} material={blurryMaterial} visible={!hide}>
       {instances}
     </Instances>
   )
