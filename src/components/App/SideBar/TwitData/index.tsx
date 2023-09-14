4f5f38a024b05ad13bcc64fb0507829dbf89b461
Расул <rasul@MacBook-Air.local>
2023-09-14 17:21:45 +0300

    feat: update tweet view

--- src/components/App/SideBar/TwitData/index.tsx
+++ src/components/App/SideBar/TwitData/index.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import { ClipLoader } from 'react-spinners'
-import { Tweet } from 'react-twitter-widgets'
+import styled from 'styled-components'
 import { Flex } from '~/components/common/Flex'
 import { Text } from '~/components/common/Text'
 import { useSelectedNode } from '~/stores/useDataStore'
@@ -8,30 +6,80 @@ import { colors } from '~/utils/colors'
 
 export const TwitData = () => {
   const selectedNode = useSelectedNode()
-  const [loading, setLoading] = useState(true)
 
   const twitId: string = selectedNode?.tweet_id || ''
 
-  return (
+  return selectedNode ? (
     <Flex direction="column" px={24} py={16}>
       <Flex align="center" direction="row" justify="flex-start" p={10}>
         <Text color="primaryText1">{selectedNode?.label}</Text>
       </Flex>
-      {loading && (
-        <Flex align="center" direction="row" justify="center" p={10}>
-          <ClipLoader color={colors.white} />
-        </Flex>
-      )}
       {twitId && (
-        <Tweet
-          onLoad={() => setLoading(false)}
-          options={{ theme: 'dark' }}
-          renderError={() => <Error />}
-          tweetId={twitId}
-        />
+        <TweetContainer>
+          <Flex direction="row">
+            <ProfilePicture>
+              <img alt="Profile" src={selectedNode.image_url || 'twitter_placeholder.png'} />
+            </ProfilePicture>
+
+            <AuthorInfo>
+              <AuthorName>{selectedNode.name}</AuthorName>
+              <TwitterHandle>{selectedNode.twitter_handle || '@unknown_handle'}</TwitterHandle>
+            </AuthorInfo>
+          </Flex>
+          <TweetText>{selectedNode.text}</TweetText>
+        </TweetContainer>
       )}
     </Flex>
-  )
+  ) : null
 }
 
-const Error = () => <Flex>Error occurred</Flex>
+const TweetContainer = styled.div`
+  padding: 10px;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  background: ${colors.BG1};
+`
+
+const ProfilePicture = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 6px;
+  overflow: hidden;
+  margin-right: 10px;
+  flex: 1 1 50px;
+  max-width: 50px;
+  margin-bottom: 16px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const AuthorInfo = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  margin-bottom: 5px;
+  flex-direction: column;
+`
+
+const AuthorName = styled.span`
+  font-family: Barlow;
+  font-weight: 700;
+  color: ${colors.white};
+`
+
+const TwitterHandle = styled.span`
+  color: ${colors.PRIMARY_BLUE};
+`
+
+const TweetText = styled.p`
+  font-family: Barlow;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 21px;
+  color: ${colors.white};
+`
