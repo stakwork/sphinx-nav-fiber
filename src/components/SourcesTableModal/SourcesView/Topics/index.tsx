de617284f44bf061dd0f562afae04c2145cfcfc4
Расул <rasul@MacBook-Air.local>
2023-10-23 14:47:31 +0300

    feat: add edge count, date, nodes to topics table

--- src/components/SourcesTableModal/SourcesView/Topics/index.tsx
+++ src/components/SourcesTableModal/SourcesView/Topics/index.tsx
@@ -1,20 +1,23 @@
-import { useEffect, useState } from 'react'
+import { Button } from '@mui/material'
+import { useEffect, useMemo, useState } from 'react'
 import { ClipLoader } from 'react-spinners'
 import styled from 'styled-components'
 import { Flex } from '~/components/common/Flex'
 import { Text } from '~/components/common/Text'
-import { Pill } from '~/components/common/Pill'
+import { DATE, DEFAULT, EDGES } from '~/constants'
 import { getTopicsData } from '~/network/fetchSourcesData'
 import { useDataStore } from '~/stores/useDataStore'
 import { FetchTopicResponse } from '~/types'
 import { colors } from '~/utils/colors'
 import { Heading } from '../common'
+import { Filter } from './Filter'
 import Table from './Table'
 
 export const TopicSources = () => {
   const [loading, setLoading] = useState(true)
   const [showMuted, setShowMuted] = useState(false)
   const [topics, setTopics] = useDataStore((s) => [s.topics, s.setTopics])
+  const [currentFilter, setCurrentFilter] = useState(DEFAULT)
 
   useEffect(() => {
     const init = async () => {
@@ -35,27 +38,45 @@ export const TopicSources = () => {
     init()
   }, [setTopics, showMuted])
 
+  const sortedNodes = useMemo(() => {
+    if (!topics) {
+      return []
+    }
+
+    if (currentFilter === DEFAULT) {
+      return topics
+    }
+
+    if (currentFilter === DATE) {
+      return [...topics].sort((a, b) =>
+        a.date_added_to_graph && b.date_added_to_graph ? a.date_added_to_graph - b.date_added_to_graph : 1,
+      )
+    }
+
+    if (currentFilter === EDGES) {
+      return [...topics].sort((a, b) => a.edgeCount - b.edgeCount)
+    }
+
+    return null
+  }, [currentFilter, topics])
+
   return (
-    <Wrapper align="stretch" direction="column" justify="flex-end">
+    <Wrapper direction="column" justify="flex-end">
       <Heading align="flex-start" justify="space-between">
         <Text className="title">Topics</Text>
       </Heading>
-      <Pill
-        className="booster__pill"
-        onClick={() => setShowMuted(!showMuted)}
-        style={{ marginLeft: '30px', marginBottom: '10px', padding: '5px 10px 5px 10px', width: 'fit-content' }}
-      >
-        {loading ? (
-          <ClipLoader color={colors.white} />
-        ) : (
-          <Flex align="center" direction="row" justify="center">
-            <div style={{ fontSize: 10 }}>Show {showMuted ? 'Unmuted' : 'Muted'}</div>
-          </Flex>
-        )}
-      </Pill>
+
+      <ActionsWrapper>
+        <Button onClick={() => setShowMuted(!showMuted)} size="medium">
+          {showMuted ? 'Show Unmuted' : 'Show Muted'}
+          {loading && <ClipLoader color={colors.BLUE_PRESS_STATE} size={10} />}
+        </Button>
+
+        <Filter currentFilter={currentFilter} onChangeFilter={setCurrentFilter} />
+      </ActionsWrapper>
 
       <TableWrapper align="center" justify={loading ? 'center' : 'flex-start'}>
-        {loading ? <ClipLoader color={colors.white} /> : <Table data={topics} showMuted={showMuted} />}
+        {loading ? <ClipLoader color={colors.white} /> : <Table data={sortedNodes || []} showMuted={showMuted} />}
       </TableWrapper>
     </Wrapper>
   )
@@ -65,7 +86,6 @@ const Wrapper = styled(Flex)`
   flex: 1;
 
   .title {
-    margin-bottom: 32px;
     font-size: 20px;
     color: ${colors.white};
     font-family: Barlow;
@@ -94,4 +114,13 @@ const TableWrapper = styled(Flex)`
   overflow: auto;
   flex: 1;
   width: 100%;
+`
+
+const ActionsWrapper = styled(Flex).attrs({
+  direction: 'row',
+  align: 'center',
+  justify: 'space-between',
+})`
+  padding: 0 36px;
+  margin-bottom: 32px;
 `
