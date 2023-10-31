5f7e2e68524172d99ff1487043e8192f276ad328
Расул <rasul@MacBook-Air.local>
2023-11-01 00:25:38 +0300

    feat: add search to topics

--- src/components/App/MainToolbar/index.tsx
+++ src/components/App/MainToolbar/index.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import AddContentIcon from '~/components/Icons/AddContentIcon'
 import AddSourceIcon from '~/components/Icons/AddSourceIcon'
 import SentimentDataIcon from '~/components/Icons/SentimentDataIcon'
-import SettingsIcon from '~/components/Icons/SettingsIcon'
 import SourcesTableIcon from '~/components/Icons/SourcesTableIcon'
 import { Flex } from '~/components/common/Flex'
 import { Text } from '~/components/common/Text'
@@ -50,12 +49,12 @@ export const MainToolbar = () => {
         </IconWrapper>
         <Text>Sentiment Data</Text>
       </ActionButton>
-      <ActionButton onClick={openSourceAddModal}>
+      {/* <ActionButton onClick={openSourceAddModal}>
         <IconWrapper>
           <SettingsIcon />
         </IconWrapper>
         <Text>Change Display</Text>
-      </ActionButton>
+      </ActionButton> */}
     </Wrapper>
   )
 }
