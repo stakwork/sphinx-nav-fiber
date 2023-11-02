4dd7a6d55340b8057307a7b6ab8cc0a2cdfc81e3
kevkevin <oapallikunnel@gmail.com>
2023-11-02 13:46:00 -0500

    test: adding new folders for utils to test, found one issue (#537)
    * test: adding new folders for utils to test, found one issue
    
    * test: updated test to just pass
    
    * fix: prettier
    
    * test: added new test for unused string

---
+++ src/components/AddContentModal/BudgetStep/__tests__/index.ts
@@ -0,0 +1,14 @@
+import { formatBudget } from '../utils'
+
+describe('formatBudget', () => {
+  /**
+   * @jest-environment jsdom
+   * @jest-environment-options  {"url": "https://jestjs.io/"}
+   * */
+  it('should assert we format the budeget properly', async () => {
+    expect(formatBudget(null)).toBe('?')
+    expect(formatBudget(25)).toBe('25')
+    expect(formatBudget(7000000)).toBe('7 000 000')
+    expect(formatBudget('700000')).toBe('700000')
+  })
+})
---
+++ src/components/AddContentModal/LocationStep/__tests__/index.ts
@@ -0,0 +1,36 @@
+import { validateLatitude, validateLongitude } from '../utils'
+
+describe('validateLongitude', () => {
+  /**
+   * @jest-environment jsdom
+   * @jest-environment-options  {"url": "https://jestjs.io/"}
+   * */
+  it('should assert we validateLongitude properly', async () => {
+    const outsideRangeErrorMsg = 'Longitude must be between -180 and 180.'
+    const needValueErrorMsg = 'Longitude is required.'
+
+    expect(validateLongitude(180)).toBe(true)
+    expect(validateLongitude(181)).toBe(outsideRangeErrorMsg)
+    expect(validateLongitude(179)).toBe(true)
+    expect(validateLongitude(0)).toBe(true)
+    expect(validateLongitude(null)).toBe(needValueErrorMsg)
+  })
+})
+
+describe('validateLatitude', () => {
+  /**
+   * @jest-environment jsdom
+   * @jest-environment-options  {"url": "https://jestjs.io/"}
+   * */
+  it('should assert we validateLatitude properly', async () => {
+    const outsideRangeErrorMsg = 'Latitude must be between -90 and 90.'
+    const needValueErrorMsg = 'Latitude is required.'
+
+    expect(validateLatitude(90)).toBe(true)
+    expect(validateLatitude(91)).toBe(outsideRangeErrorMsg)
+    expect(validateLatitude(89)).toBe(true)
+    expect(validateLatitude(0)).toBe(true)
+    expect(validateLatitude(null)).toBe(true)
+    expect(validateLatitude('abc')).toBe(needValueErrorMsg)
+  })
+})
