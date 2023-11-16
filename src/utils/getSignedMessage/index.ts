49f8cf1dbb41174ded2cd534a0ff3fb23f478e81
Oluwatobi Bamidele <teebams49@gmail.com>
2023-11-16 22:10:33 +0100

    feat: sign message function

---
+++ src/utils/getSignedMessage/index.ts
@@ -0,0 +1,19 @@
+import * as sphinx from 'sphinx-bridge-kevkevinpal'
+
+export async function getSignedMessageFromRelay(): Promise<{ message: string; signature: string }> {
+  const message = `${window.crypto.randomUUID()}${new Date().getTime()}`
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const isSphinx = await sphinx.enable()
+
+  if (isSphinx) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const storedLsat = await sphinx.signMessage(window.location.host)
+
+    return { message, signature: storedLsat.signature }
+  }
+
+  return { message: '', signature: '' }
+}
