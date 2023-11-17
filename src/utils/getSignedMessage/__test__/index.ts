964d85feb95fe436af04f20f2f151f40d5044966
Oluwatobi Bamidele <teebams49@gmail.com>
2023-11-17 01:52:54 +0100

    test: added test for getSignedMessageFromRelay function

---
+++ src/utils/getSignedMessage/__test__/index.ts
@@ -0,0 +1,11 @@
+import { getSignedMessageFromRelay } from '..'
+
+describe('assertNever', () => {
+  /**
+   * @jest-environment jsdom
+   * @jest-environment-options  {"url": "https://jestjs.io/"}
+   * */
+  it('should return empty message and empty signature', async () => {
+    expect(await getSignedMessageFromRelay()).toBe({ message: '', signature: '' })
+  })
+})
