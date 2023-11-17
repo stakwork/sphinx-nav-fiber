909463235428e3a1dd0e84c9e5aeadb21bc3b5e8
Oluwatobi Bamidele <teebams49@gmail.com>
2023-11-17 03:05:36 +0100

    test: added test to auth network

---
+++ src/network/auth/__tests__/index.ts
@@ -0,0 +1,22 @@
+import { getIsAdmin } from '..'
+import { api } from '../../api'
+
+describe('test extracting uuid and tribe host', () => {
+  // Should make a GET request to the correct endpoint with the correct query parameters
+  it('should make a GET request to the correct endpoint with the correct query parameters', () => {
+    const mockApiGet = jest.spyOn(api, 'get')
+
+    const authRequest = {
+      message: 'test message',
+      tribeHost: 'test tribe host',
+      tribeUuid: 'test tribe uuid',
+      signature: 'test signature',
+    }
+
+    const expectedEndpoint = `/isAdmin?msg=${authRequest.message}&sig=${authRequest.signature}&tribe_host=${authRequest.tribeHost}&uuid=${authRequest.tribeUuid}`
+
+    getIsAdmin(authRequest)
+
+    expect(mockApiGet).toHaveBeenCalledWith(expectedEndpoint)
+  })
+})
