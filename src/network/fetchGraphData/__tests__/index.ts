46c4b638e2cd0df9fd5dea01eb57bf414c697d65
Rassl <rasul.sahibjanli@gmail.com>
2023-11-20 18:21:16 +0300

    feat: add twitt connection to topics and guests (#605)
    * feat: add twitt connection to topics and guests
    
    * feat: add unit test for topic nodes
    
    * fix: rollback removed unit tests
    
    ---------
    
    Co-authored-by: Расул <rasul@MacBook-Air.local>

--- src/network/fetchGraphData/__tests__/index.ts
+++ src/network/fetchGraphData/__tests__/index.ts
@@ -10,6 +10,27 @@ describe('formatFetchNodes', () => {
     node_type: 'test',
   }
 
+  const tweetNode = {
+    date: 1700158742,
+    latitude: null,
+    longitude: null,
+    name: 'Yan | swan.com',
+    node_type: 'tweet',
+    posted_by: {
+      name: 'Marty Bent',
+      ref_id: '655bdcb1-21c3-411c-aa24-fe7e4c7a248c',
+      twitter_handle: 'MartyBent',
+    },
+    profile_picture: 'https://pbs.twimg.com/profile_images/1599281471679766530/RhszStuB_normal.jpg',
+    ref_id: 'a1317497-7ebe-401b-8cfd-fe709bf04e46',
+    text: '@adam3us @JSeyff @SGJohnsson Where’s the Vitalik presale video where he talks about finding a friendly jurisdiction?',
+    topics: ['Vitalik', 'Bitcoin', 'presale', 'friendly jurisdiction'],
+    tweet_id: '1725216924051587516',
+    twitter_handle: 'skwp',
+    verified: false,
+    weight: 2,
+  }
+
   const dataSeriesNode = {
     x: 1,
     y: 1,
@@ -115,5 +136,17 @@ describe('formatFetchNodes', () => {
     expect(formattedResponse.nodes[2].scale).toBe(1.5)
     expect(formattedResponse.nodes[2].type).toBe('data_series')
     expect(formattedResponse.nodes[2].index).toBe(2)
+  })
+
+  it('should create topic nodes for each topic from tweet, and guest node for posted_by', () => {
+    const emptyInput = {
+      data_series: { title: 'test' },
+      exact: [tweetNode],
+      related: [],
+    }
+
+    const result = formatFetchNodes(emptyInput, '1', 'split')
+
+    expect(result.nodes.length).toBe(tweetNode.topics.length + 2)
   })
 })
