79a613c714035f1a5cec11d9080f82e48b979eb6
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-12-24 19:38:41 +0100

    feat: created useIsMobile logic in a hook

---
+++ src/hooks/useIsMobile/index.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+const compatibilityBreakPoint = 620
+
+export const useIsMobile = () => {
+  const query = `(max-width: ${compatibilityBreakPoint}px)`
+
+  const [isMobile, setIsMobile] = useState(() => {
+    const mediaQuery = window.matchMedia(query)
+
+    return mediaQuery.matches
+  })
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query)
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleScreenResize = () => {
+      setIsMobile(mediaQuery.matches)
+    }
+
+    window.addEventListener('resize', handleScreenResize)
+
+    return () => mediaQuery.removeEventListener('change', handleScreenResize)
+  }, [query])
+
+  return [isMobile, setIsMobile] as const
+}
