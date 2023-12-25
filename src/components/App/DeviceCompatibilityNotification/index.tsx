a941d85a4c43358ae6ebc4db52b34bf9a645da42
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-12-25 22:21:13 +0100

    chore: refactored component

--- src/components/App/DeviceCompatibilityNotification/index.tsx
+++ src/components/App/DeviceCompatibilityNotification/index.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@mui/material'
 import styled from 'styled-components'
-import { Button } from '~/components/Button'
+import { Flex } from '~/components/common/Flex'
+import { Text } from '~/components/common/Text'
 import { useIsMobile } from '~/hooks/useIsMobile'
 
 export const DeviceCompatibilityNotice = () => {
@@ -10,42 +12,33 @@ export const DeviceCompatibilityNotice = () => {
   }
 
   return isMobile ? (
-    <Wrapper onClick={handleClick}>
+    <Wrapper align="center" direction="column" justify="center" onClick={handleClick}>
       <img alt="screen" src="jamboard_kiosk.png" />
-      <TextWrapper>
+      <Flex align="center" direction="column" justify="center">
         <StyledText>Second Brain is currently</StyledText>
         <StyledText style={{ fontWeight: 600 }}>optimized for Desktop.</StyledText>
         <StyledText>Mobile support coming soon.</StyledText>
-      </TextWrapper>
-      <StyledButton onClick={handleClick}>Got It</StyledButton>
+      </Flex>
+      <Button color="secondary" onClick={handleClick} variant="contained">
+        Got It
+      </Button>
     </Wrapper>
   ) : null
 }
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
+const Wrapper = styled(Flex)`
   height: 100vh;
   width: 100vw;
   background: rgba(0, 0, 0, 0.75);
   gap: 28px;
-  z-index: 2147483647;
+  z-index: 2000000000;
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
 `
 
-const TextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
-
-const StyledText = styled.p`
+const StyledText = styled(Text)`
   color: #fff;
   text-align: center;
   font-family: Barlow;
@@ -54,18 +47,4 @@ const StyledText = styled.p`
   font-weight: 400;
   line-height: 24px;
   margin: 1px;
-`
-
-const StyledButton = styled(Button)`
-  display: flex;
-  width: 115px;
-  height: 40px;
-  padding: 12px 16px;
-  justify-content: center;
-  align-items: center;
-  gap: 12px;
-  flex-shrink: 0;
-  border-radius: 6px;
-  background: var(--Primary-Blue, #618aff);
-  margin-top: 14px;
 `
