05f60646aa0d76911e7138b570fb50ffd0fed56a
gouravmpk <gouravmpk@gmail.com>
2024-01-03 23:03:20 +0530

    Added RSS feed to regex check #727

--- src/components/AddContentModal/utils/__tests__/index.ts
+++ src/components/AddContentModal/utils/__tests__/index.ts
@@ -1,4 +1,4 @@
-import { DOCUMENT, LINK, TWITTER_HANDLE, TWITTER_SOURCE, WEB_PAGE, YOUTUBE_CHANNEL } from '~/constants'
+import { DOCUMENT, LINK, TWITTER_HANDLE, TWITTER_SOURCE, WEB_PAGE, YOUTUBE_CHANNEL,RSS } from '~/constants'
 import { extractNameFromLink, getInputType } from '..'
 
 describe('youtubeRegex', () => {
@@ -83,3 +83,13 @@ describe('extractNameFromLink', () => {
     expect(result).toBeNull()
   })
 })
+
+describe('getInputType', () => {
+  it('should assert we can check for RSS feed url regex', async () => {
+    expect(getInputType('http://example.com/feed')).toBe(RSS);
+    expect(getInputType('http://example.com/rss')).toBe(RSS);
+    expect(getInputType('https://example.com/news.rss')).toBe(RSS);
+    expect(getInputType('http://example.com/rss.xml')).toBe(RSS);
+    expect(getInputType('http://example.com/?feed=rss')).toBe(RSS);
+  });
+});
