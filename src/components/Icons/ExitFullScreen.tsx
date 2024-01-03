090831b53a8c783f93ffa51db59c45230ececc98
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-04 02:24:16 +0530

    fix(#726): enable fullscreen for videos

---
+++ src/components/Icons/ExitFullScreen.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React from 'react'
+
+const ExitFullScreen: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+    <mask id="mask0_4160_9271" maskUnits="userSpaceOnUse" x="0" y="0" width="32" height="32">
+      <rect width="32" height="32" fill="#D9D9D9" />
+    </mask>
+    <g mask="url(#mask0_4160_9271)">
+      <path
+        d="M11 25V21H7V19H13V25H11ZM19 25V19H25V21H21V25H19ZM7 13V11H11V7H13V13H7ZM19 13V7H21V11H25V13H19Z"
+        fill="white"
+      />
+    </g>
+  </svg>
+)
+
+export default ExitFullScreen
