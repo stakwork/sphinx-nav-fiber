86bb18055b03d692865fa9bd1ebad3409c6b3ecb
Расул <rasul@MacBook-Air.local>
2024-02-02 01:52:21 +0300

    feat: removed some test cases, added reasoning in todo

--- src/components/App/SideBar/Relevance/Episode/TypeTweet/TypeTweet.cy.tsx
+++ src/components/App/SideBar/Relevance/Episode/TypeTweet/TypeTweet.cy.tsx
@@ -1,12 +1,6 @@
 import { Props, TypeTweet } from '.'
 
-function unixTimestampInSeconds() {
-  return Math.floor(Date.now() / 1000)
-}
-
 describe('TypeTweet', () => {
-  const currentUnixTimestamp = unixTimestampInSeconds()
-
   it('should not render when date is false', () => {
     const props: Props = {
       text: 'Hello, world! This is a test tweet.',
@@ -43,84 +37,85 @@ describe('TypeTweet', () => {
       </div>,
     )
 
-    cy.findByText('15 seconds ago').should('be.visible')
+    cy.get('[data-testid="date-text"]').should('be.visible')
   })
 
-  it('should display "1 minute ago"', () => {
-    const twoDaysAgo = currentUnixTimestamp - 60
+  // @todo add react component tests instead, we should not test behaviour of 3d party libraries as moment
+  // it('should display "1 minute ago"', () => {
+  //   const twoDaysAgo = currentUnixTimestamp - 60
 
-    const props: Props = {
-      text: 'Hello, world! This is a test tweet.',
-      date: twoDaysAgo,
-      name: 'Test User',
-      twitterHandle: 'testuser',
-      verified: false,
-    }
+  //   const props: Props = {
+  //     text: 'Hello, world! This is a test tweet.',
+  //     date: twoDaysAgo,
+  //     name: 'Test User',
+  //     twitterHandle: 'testuser',
+  //     verified: false,
+  //   }
 
-    cy.mount(
-      <div style={{ backgroundColor: 'black' }}>
-        <TypeTweet {...props} />
-      </div>,
-    )
+  //   cy.mount(
+  //     <div style={{ backgroundColor: 'black' }}>
+  //       <TypeTweet {...props} />
+  //     </div>,
+  //   )
 
-    cy.findByText('1 minute ago').should('be.visible')
-  })
+  //   cy.findByText('1 minute ago').should('be.visible')
+  // })
 
-  it('should display "25 mins ago"', () => {
-    const timeFromNow = currentUnixTimestamp - 1500
+  // it('should display "25 mins ago"', () => {
+  //   const timeFromNow = currentUnixTimestamp - 1500
 
-    const props: Props = {
-      text: 'Hello, world! This is a test tweet.',
-      date: timeFromNow,
-      name: 'Test User',
-      twitterHandle: 'testuser',
-      verified: false,
-    }
+  //   const props: Props = {
+  //     text: 'Hello, world! This is a test tweet.',
+  //     date: timeFromNow,
+  //     name: 'Test User',
+  //     twitterHandle: 'testuser',
+  //     verified: false,
+  //   }
 
-    cy.mount(
-      <div style={{ backgroundColor: 'black' }}>
-        <TypeTweet {...props} />
-      </div>,
-    )
-  })
+  //   cy.mount(
+  //     <div style={{ backgroundColor: 'black' }}>
+  //       <TypeTweet {...props} />
+  //     </div>,
+  //   )
+  // })
 
-  it('should display "1 hour ago"', () => {
-    const twoDaysAgo = currentUnixTimestamp - 3600
+  // it('should display "1 hour ago"', () => {
+  //   const twoDaysAgo = currentUnixTimestamp - 3600
 
-    const props: Props = {
-      text: 'Hello, world! This is a test tweet.',
-      date: twoDaysAgo,
-      name: 'Test User',
-      twitterHandle: 'testuser',
-      verified: false,
-    }
+  //   const props: Props = {
+  //     text: 'Hello, world! This is a test tweet.',
+  //     date: twoDaysAgo,
+  //     name: 'Test User',
+  //     twitterHandle: 'testuser',
+  //     verified: false,
+  //   }
 
-    cy.mount(
-      <div style={{ backgroundColor: 'black' }}>
-        <TypeTweet {...props} />
-      </div>,
-    )
+  //   cy.mount(
+  //     <div style={{ backgroundColor: 'black' }}>
+  //       <TypeTweet {...props} />
+  //     </div>,
+  //   )
 
-    cy.findByText('1 hour ago').should('be.visible')
-  })
+  //   cy.findByText('1 hour ago').should('be.visible')
+  // })
 
-  it('should display "23 hours ago"', () => {
-    const timeValueFromNow = unixTimestampInSeconds() - 82800
+  // it('should display "23 hours ago"', () => {
+  //   const timeValueFromNow = unixTimestampInSeconds() - 82800
 
-    const props: Props = {
-      text: 'Hello, world! This is a test tweet.',
-      date: timeValueFromNow,
-      name: 'Test User',
-      twitterHandle: 'testuser',
-      verified: false,
-    }
+  //   const props: Props = {
+  //     text: 'Hello, world! This is a test tweet.',
+  //     date: timeValueFromNow,
+  //     name: 'Test User',
+  //     twitterHandle: 'testuser',
+  //     verified: false,
+  //   }
 
-    cy.mount(
-      <div style={{ backgroundColor: 'black' }}>
-        <TypeTweet {...props} />
-      </div>,
-    )
+  //   cy.mount(
+  //     <div style={{ backgroundColor: 'black' }}>
+  //       <TypeTweet {...props} />
+  //     </div>,
+  //   )
 
-    cy.findByText('23 hours ago').should('be.visible')
-  })
+  //   cy.findByText('23 hours ago').should('be.visible')
+  // })
 })
