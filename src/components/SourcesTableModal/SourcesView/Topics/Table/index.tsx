36fb24d07313d2a49e624b9a3322ce072ee9ffea
MahtabBukhari <mahtabali50447@gmail.com>
2024-03-07 09:17:48 +0500

    fix(update-and-move-sort-by-component-and-logic): update and move sort by component

--- src/components/SourcesTableModal/SourcesView/Topics/Table/index.tsx
+++ src/components/SourcesTableModal/SourcesView/Topics/Table/index.tsx
@@ -7,21 +7,52 @@ import { Flex } from '~/components/common/Flex'
 import { Text } from '~/components/common/Text'
 import { useAppStore } from '~/stores/useAppStore'
 import { useModal } from '~/stores/useModalStore'
-import { useTopicsStore } from '~/stores/useTopicsStore'
+// import { useTopicsStore } from '~/stores/useTopicsStore'
 import { colors } from '~/utils/colors'
 import { StyledTableCell, StyledTableHead } from '../../common'
 import { IS_ALIAS } from '../../constants'
 import { TopicTableProps } from '../../types'
 import { TopicRow } from './TableRow'
+import { Topic } from '~/types'
+import { MdSort } from 'react-icons/md'
 
-export const Table: React.FC<TopicTableProps> = ({ showMuted, onTopicEdit }) => {
+export const Table: React.FC<TopicTableProps> = ({ showMuted, onTopicEdit, onChangeFilter }) => {
   const { close } = useModal('sourcesTable')
 
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
   const [selectedRefId, setSelectedRefId] = React.useState<string>('')
 
   const [setSearchFormValue, setCurrentSearch] = useAppStore((s) => [s.setSearchFormValue, s.setCurrentSearch])
-  const [data, ids] = useTopicsStore((s) => [s.data, s.ids])
+  // const [data, ids] = useTopicsStore((s) => [s.data, s.ids])
+
+  const data: Topic[] = [
+    {
+      topic: 'Privacy Confirmation',
+      edgeCount: 4830,
+      edgeList: ['RELATED_TO', 'IS_ALIAS', 'MENTIONS', 'IS_SUMMARIZED'],
+      date_added_to_graph: '1644115200',
+      muted_topic: 'false',
+      ref_id: '0',
+    },
+    {
+      topic: 'Absence of Documents',
+      edgeCount: 630,
+      edgeList: ['RELATED_TO', 'IS_ALIAS', 'MENTIONS', 'IS_SUMMARIZED'],
+      date_added_to_graph: '1644115200',
+      muted_topic: 'true',
+      ref_id: '1',
+    },
+    {
+      topic: 'Year 2009',
+      edgeCount: 130,
+      edgeList: ['RELATED_TO', 'IS_ALIAS', 'MENTIONS', 'IS_SUMMARIZED'],
+      date_added_to_graph: '1644115200',
+      muted_topic: 'true',
+      ref_id: '2',
+    },
+  ]
+
+  const ids: string[] = data.map((topic, index) => String(index))
 
   const handleClick = useCallback((event: React.MouseEvent<HTMLButtonElement>, refId: string) => {
     setAnchorEl(event.currentTarget)
@@ -41,6 +72,10 @@ export const Table: React.FC<TopicTableProps> = ({ showMuted, onTopicEdit }) => 
   const handlePopoverAction = (action: string) => {
     onTopicEdit(selectedRefId, action)
     handleClose()
+  }
+
+  const handleChange = (option: string) => {
+    onChangeFilter(option)
   }
 
   const open = Boolean(anchorEl)
@@ -64,10 +99,22 @@ export const Table: React.FC<TopicTableProps> = ({ showMuted, onTopicEdit }) => 
             <StyledTableHead>
               <TableRow component="tr">
                 <StyledTableCell className="empty" />
-                <StyledTableCell>Type</StyledTableCell>
-                <StyledTableCell>Edge Count</StyledTableCell>
+                <StyledTableCell>
+                  <SortedIcon onClick={() => handleChange('Alphabetically')}>
+                    Name <MdSort color={colors.GRAY7} fontSize={16} />
+                  </SortedIcon>
+                </StyledTableCell>
+                <StyledTableCell>
+                  <SortedIcon onClick={() => handleChange('Edge Count')}>
+                    Count <MdSort color={colors.GRAY7} fontSize={16} />
+                  </SortedIcon>
+                </StyledTableCell>
                 <StyledTableCell>Edge list</StyledTableCell>
-                <StyledTableCell>Date</StyledTableCell>
+                <StyledTableCell>
+                  <SortedIcon onClick={() => handleChange('Date')}>
+                    Date <MdSort color={colors.GRAY7} fontSize={16} />
+                  </SortedIcon>
+                </StyledTableCell>
                 <StyledTableCell>{showMuted ? 'Unmute' : 'Mute'}</StyledTableCell>
                 <StyledTableCell />
               </TableRow>
@@ -75,7 +122,7 @@ export const Table: React.FC<TopicTableProps> = ({ showMuted, onTopicEdit }) => 
             {data && (
               <tbody>
                 {ids?.map((i: string) => (
-                  <TopicRow key={i} onClick={handleClick} onSearch={handleSearch} topic={data[i]} />
+                  <TopicRow key={i} onClick={handleClick} onSearch={handleSearch} topic={data[parseInt(i, 10)]} />
                 ))}
               </tbody>
             )}
@@ -90,7 +137,7 @@ export const Table: React.FC<TopicTableProps> = ({ showMuted, onTopicEdit }) => 
               transformOrigin={{ vertical: 'top', horizontal: 'right' }}
             >
               <PopoverOption onClick={() => handlePopoverAction('editTopic')}>Rename</PopoverOption>
-              {!data[selectedRefId].edgeList.includes(IS_ALIAS) ? (
+              {!data[parseInt(selectedRefId, 10)].edgeList.includes(IS_ALIAS) ? (
                 <PopoverOption onClick={() => handlePopoverAction('mergeTopic')}>Merge</PopoverOption>
               ) : null}
               <PopoverOption onClick={() => handlePopoverAction('addEdge')}>Add edge</PopoverOption>
@@ -101,6 +148,13 @@ export const Table: React.FC<TopicTableProps> = ({ showMuted, onTopicEdit }) => 
     </>
   )
 }
+
+const SortedIcon = styled.span`
+  cursor: pointer;
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`
 
 const PopoverOption = styled(Flex).attrs({
   direction: 'row',
