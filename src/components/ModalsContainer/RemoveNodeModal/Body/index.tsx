7083ef6ff210009b79d437a919680be4944f264a
airmusdev <airmus.dev@gmail.com>
2024-03-10 23:45:11 +0800

    feat: new edit node componet

--- src/components/ModalsContainer/RemoveNodeModal/Body/index.tsx
+++ src/components/ModalsContainer/RemoveNodeModal/Body/index.tsx
@@ -2,6 +2,7 @@ import { Button, Skeleton } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { ClipLoader } from 'react-spinners'
 import { TypeOptions, toast } from 'react-toastify'
+import styled from 'styled-components'
 import { Flex } from '~/components/common/Flex'
 import { ToastMessage } from '~/components/common/Toast/toastMessage'
 import { deleteNode, getTopicsData } from '~/network/fetchSourcesData'
@@ -25,6 +26,8 @@ const notify = (message: string, type: TypeOptions) => {
 
 export const Body = () => {
   const { close } = useModal('removeNode')
+  const { close: closeEditNodeModal } = useModal('editNodeName')
+
   const [loading, setLoading] = useState(false)
   const [removeNode, setSelectedNode] = useDataStore((s) => [s.removeNode, s.setSelectedNode])
 
@@ -74,6 +77,7 @@ export const Body = () => {
       notify('Topic node removal coming soon', 'info')
       setSelectedNode(null)
       closeHandler()
+      closeEditNodeModal()
     } catch (error) {
       console.warn(error)
     } finally {
@@ -102,6 +106,7 @@ export const Body = () => {
       setSelectedNode(null)
       notify('Removed Node', 'success')
       closeHandler()
+      closeEditNodeModal()
     } catch (error) {
       console.log(error)
       notify('Removed failed, try later', 'error')
@@ -113,26 +118,67 @@ export const Body = () => {
   }
 
   return (
-    <>
+    <Wrapper>
       <TitleEditor />
       {topicIsLoading ? (
         <Skeleton />
       ) : (
-        <Flex direction="row" justify="space-between">
-          <Button color="secondary" onClick={closeHandler} size="medium" variant="outlined">
-            Cancel
-          </Button>
-          <Button
-            disabled={loading || (!actualNode && !actualTopicNode)}
-            onClick={actualNode || actualTopicNode ? handleRemove : handleTopicRemove}
-            size="medium"
-            variant="text"
+        <Flex direction="row" mt={34}>
+          <CancelButton
+            color="secondary"
+            onClick={closeHandler}
+            size="large"
+            style={{ flex: 1, marginRight: 20 }}
+            variant="contained"
           >
-            Confirm
+            Cancel
+          </CancelButton>
+          <DeleteButton
+            color="secondary"
+            // TODO
+            // disabled={loading || (!actualNode && !actualTopicNode)}
+            onClick={actualNode || actualTopicNode ? handleRemove : handleTopicRemove}
+            size="large"
+            style={{ flex: 1 }}
+            variant="contained"
+          >
+            Delete
             {loading && <ClipLoader color={colors.BLUE_PRESS_STATE} size={10} />}
-          </Button>
+          </DeleteButton>
         </Flex>
       )}
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled(Flex)`
+  padding: 4px 12px 16px;
+`
+
+const CancelButton = styled(Button)`
+  && {
+    background: ${colors.white};
+    color: ${colors.BG2};
+
+    &:active,
+    &:hover,
+    &:focus {
+      background: ${colors.white};
+      color: ${colors.BG2};
+    }
+  }
+`
+
+const DeleteButton = styled(Button)`
+  && {
+    color: ${colors.white};
+    background-color: ${colors.primaryRed};
+
+    &:hover,
+    &:active,
+    &:focus {
+      color: ${colors.white};
+      background-color: ${colors.primaryRed};
+    }
+  }
+`
--- src/components/ModalsContainer/RemoveNodeModal/Title/index.tsx
+++ src/components/ModalsContainer/RemoveNodeModal/Title/index.tsx
@@ -1,19 +1,40 @@
 import styled from 'styled-components'
 import { Flex } from '~/components/common/Flex'
-import { Text } from '~/components/common/Text'
+import DeleteNodeIcon from '~/components/Icons/DeleteNodeIcon'
+import { colors } from '~/utils/colors'
 
 export const TitleEditor = () => (
   <Flex>
     <Flex align="center" direction="row" justify="space-between" mb={18}>
-      <Flex align="center" direction="row">
-        <StyledText>Remove node</StyledText>
+      <Flex align="center" direction="column">
+        <IconWrapper>
+          <DeleteNodeIcon />
+        </IconWrapper>
+        <Title>Are you sure you want to delete this item?</Title>
       </Flex>
     </Flex>
   </Flex>
 )
 
-const StyledText = styled(Text)`
-  font-size: 22px;
-  font-weight: 600;
+const Title = styled(Flex)`
+  color: ${colors.white};
   font-family: 'Barlow';
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 30px;
+  letter-spacing: 0px;
+  text-align: center;
+  padding: 0 20px;
+`
+
+const IconWrapper = styled(Flex)`
+  justify-content: center;
+  align-items: center;
+  font-size: 52px;
+  color: #23252f;
+  margin-bottom: 20px;
+
+  path:nth-child(3) {
+    color: #6b7a8d;
+  }
 `
