f4610884fdc4fab356eb82ec89bc98980f8bc099
Mirza Hanan <mirzahh07@gmail.com>
2024-03-11 22:02:48 +0500

    fix(add-icons): update dropdown to include mute and new icons

---
+++ src/components/Icons/MergeIcon.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react'
+
+const MergeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg width="20" height="15" viewBox="-3 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M1.33317 15.5L0.166504 14.3333L4.18734 10.2917C4.50678 9.97222 4.74984 9.61111 4.9165 9.20833C5.08317 8.80556 5.1665 8.38194 5.1665 7.9375L5.1665 3.6875L3.83317 5L2.6665 3.83333L5.99984 0.5L9.33317 3.83333L8.1665 5L6.83317 3.6875L6.83317 7.9375C6.83317 8.38194 6.9165 8.80556 7.08317 9.20833C7.24984 9.61111 7.49289 9.97222 7.81234 10.2917L11.8332 14.3333L10.6665 15.5L5.99984 10.8333L1.33317 15.5Z"
+      fill="white"
+    />
+  </svg>
+)
+
+export default MergeIcon
