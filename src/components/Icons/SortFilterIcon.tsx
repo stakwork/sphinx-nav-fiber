915770c8d32f89b8b5547d80cfc7f060fc133133
MahtabBukhari <mahtabali50447@gmail.com>
2024-03-12 14:39:55 +0500

    fix(boost-button-working): fixed

--- src/components/Icons/SortFilterIcon.tsx
+++ src/components/Icons/SortFilterIcon.tsx
@@ -2,14 +2,14 @@
 import React from 'react'
 
 const SortFilterIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg width="1.4em" height="1.4em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <mask id="mask0_5382_33739" maskUnits="userSpaceOnUse" x="0" y="0" width="16" height="16">
-      <rect width="1.5em" height="1.5em" fill="currentColor" />
+  <svg width="1em" height="1em" viewBox="2 2 10 10" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <mask id="mask0_5382_33739" maskUnits="userSpaceOnUse" x="0" y="0" width="30" height="30">
+      <rect width="1em" height="1em" fill="currentColor" />
     </mask>
     <g mask="url(#mask0_5382_33739)">
       <path
         d="M2.3335 11.667V10.6671H5.91681V11.667H2.3335ZM2.3335 8.50037V7.50041H9.7886V8.50037H2.3335ZM2.3335 5.33372V4.33374H13.6668V5.33372H2.3335Z"
-        fill="gray"
+        fill="currentColor"
       />
     </g>
   </svg>
