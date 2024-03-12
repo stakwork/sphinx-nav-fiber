b2af3c9d82f97a44b408ab976575136346d6c04c
Vayras <89837102+Vayras@users.noreply.github.com>
2024-03-13 01:24:33 +0530

    increased graph description character limit

--- src/components/SettingsModal/SettingsView/General/index.tsx
+++ src/components/SettingsModal/SettingsView/General/index.tsx
@@ -53,7 +53,7 @@ export const General: FC<Props> = ({ initialValues }) => {
               <TextInput
                 id="cy-about-id"
                 label="Graph Description"
-                maxLength={100}
+                maxLength={50}
                 name="description"
                 placeholder="Type graph description here..."
               />
