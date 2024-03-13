9d26b22d51fcb06ebcdcf0869de95edd6425ec3c
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-03-13 16:15:04 +0530

    Update index.tsx

--- src/components/common/SuccessToast/index.tsx
+++ src/components/common/SuccessToast/index.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable react/button-has-type */
 import { MdCheckCircle } from 'react-icons/md'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import styled from 'styled-components'
-import { NODE_ADD_SUCCESS } from '~/constants'
 import { colors } from '~/utils/colors'
 
 const StyledContainer = styled(ToastContainer)`
@@ -17,6 +15,9 @@ const StyledContainer = styled(ToastContainer)`
     width: 200px;
     opacity: 200;
     font-family: Barlow;
+    position: absolute;
+    top: 60px;
+    right: 35px;
   }
   .Toastify__close-button {
     display: none;
@@ -30,13 +31,43 @@ const StyledDiv = styled.div`
   gap: 5px;
 `
 
-export const SuccessNotify = (message: string) =>
-  toast(
-    <StyledDiv>
-      <MdCheckCircle color={colors.white} fontSize={24} />
-      {message === NODE_ADD_SUCCESS ? 'Source Submitted!' : null}
-    </StyledDiv>,
-  )
+export const SuccessNotify = (isAdmin?: boolean, type?: string) => {
+  let toastMessage = ''
+  const toastId = 'uniqueToastId'
+
+  switch (type) {
+    case 'source':
+      toastMessage = 'Source Added'
+      break
+    case 'addContent':
+      toastMessage = 'Content Submitted'
+      break
+    case 'additem':
+      if (isAdmin) {
+        toastMessage = 'Item Added'
+      }
+
+      break
+    case 'edge':
+      toastMessage = 'Link Created'
+      break
+    default:
+      toastMessage = 'Success'
+  }
+
+  if (!toast.isActive(toastId)) {
+    toast(
+      <StyledDiv>
+        <MdCheckCircle color={colors.white} fontSize={24} />
+        {toastMessage}
+      </StyledDiv>,
+      {
+        toastId,
+        autoClose: 5000,
+      },
+    )
+  }
+}
 
 export const Toast = () => (
   <div>
