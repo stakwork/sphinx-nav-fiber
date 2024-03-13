984c533f95f5442e0925caab13bb402bd517a53d
aliraza556 <alirazaakhtar1122@gmail.com>
2024-03-13 15:09:27 +0500

    fix(added-schema): small changes

--- src/stores/useFeatureFlagStore/index.ts
+++ src/stores/useFeatureFlagStore/index.ts
@@ -3,22 +3,22 @@ import { create } from 'zustand'
 export type FeatureFlagStore = {
   trendingTopicsFlag: boolean
   queuedSourcesFlag: boolean
-  FeatureFlag: boolean
+  customSchema: boolean
   v2Flag: boolean
   setTrendingTopicsFlag: (val: boolean) => void
   setV2Flag: (val: boolean) => void
   setQueuedSourcesFlag: (val: boolean) => void
-  setFeatureFlag: (val: boolean) => void
+  setCustomSchema: (val: boolean) => void
 }
 
 const defaultData: Omit<
   FeatureFlagStore,
-  'setTrendingTopicsFlag' | 'setV2Flag' | 'setQueuedSourcesFlag' | 'setFeatureFlag'
+  'setTrendingTopicsFlag' | 'setV2Flag' | 'setQueuedSourcesFlag' | 'setCustomSchema'
 > = {
   trendingTopicsFlag: true,
   queuedSourcesFlag: false,
   v2Flag: false,
-  FeatureFlag: false,
+  customSchema: false,
 }
 
 export const useFeatureFlagStore = create<FeatureFlagStore>((set) => ({
@@ -26,5 +26,5 @@ export const useFeatureFlagStore = create<FeatureFlagStore>((set) => ({
   setTrendingTopicsFlag: (trendingTopicsFlag) => set({ trendingTopicsFlag }),
   setV2Flag: (v2Flag) => set({ v2Flag }),
   setQueuedSourcesFlag: (queuedSourcesFlag) => set({ queuedSourcesFlag }),
-  setFeatureFlag: (FeatureFlag) => set({ FeatureFlag }),
+  setCustomSchema: (customSchema) => set({ customSchema }),
 }))
