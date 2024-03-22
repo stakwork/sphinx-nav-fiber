315e549ea1704da40c62bbc04f712d65c8ea1075
Oluwatobi Bamidele <teebams49@gmail.com>
2024-03-22 23:37:28 +0100

    feat: added adding twitter source cypress test

---
+++ cypress/e2e/addContent/addSource.cy.ts
@@ -0,0 +1,23 @@
+describe('Add Tweeter Handler', () => {
+  it('Carol adds tweet handle to graph', () => {
+    cy.visit('http://localhost:3000', {
+      onBeforeLoad(win) {
+        // @ts-ignore
+        win.CYPRESS_USER = 'carol'
+
+        // @ts-ignore
+        win.CYPRESS_USER_BUDGET = 50
+      },
+    })
+
+    cy.wait(5000)
+    cy.get('[data-testid="add-content-modal"]').click()
+    cy.get('[id="cy-youtube-channel-id"]').type('https://twitter.com/PhoenixWallet')
+    cy.get('[data-testid="add-content-btn"]').click()
+    cy.get('[data-testid="source_next_btn"]').click()
+    cy.get('[data-testid="check-icon"]').click()
+
+    cy.wait(7000)
+    cy.get('.Toastify__toast-body').should('have.text', 'Content Added')
+  })
+})
