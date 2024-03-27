8f493fa4bd14cb25ea34b3741de2ef81eecfbea3
Расул <rasul.sahibjanli@gmail.com>
2024-03-27 14:30:57 +0300

    fix: fix unit test

--- src/components/AddTypeModal/utils/__tests__/index.ts
+++ src/components/AddTypeModal/utils/__tests__/index.ts
@@ -56,7 +56,7 @@ describe('parseJson function', () => {
   it('should parse JSON object properly when some attributes are optional', () => {
     const input = {
       name: 'string',
-      age: 'number',
+      age: '?number',
     }
 
     const output = parseJson(input)
