cdc2d1e4c82f2a32b248b8f560d6b0ee212bec24
Расул <rasul.sahibjanli@gmail.com>
2024-04-04 19:18:34 +0300

    feat: test unit run

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -51,6 +51,8 @@ export const App = () => {
   const [setBudget, setNodeCount] = useUserStore((s) => [s.setBudget, s.setNodeCount])
   const [isLoading, setIsLoading] = useState(false)
 
+  console.log(isLoading)
+
   const {
     setSidebarOpen,
     currentSearch: searchTerm,
