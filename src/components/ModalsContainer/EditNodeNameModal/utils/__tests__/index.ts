b6a096f4199a3320206a722e0c963b97eff03159
MahtabBukhari <mahtabali50447@gmail.com>
2024-04-07 06:40:11 +0500

    fix(image-for-any-node): update unit test

--- src/components/ModalsContainer/EditNodeNameModal/utils/__tests__/index.ts
+++ src/components/ModalsContainer/EditNodeNameModal/utils/__tests__/index.ts
@@ -2,14 +2,24 @@ import { validateImageInputType } from '../index'
 
 describe('validateImageInputType function', () => {
   it('should return true for valid image URLs', () => {
-    expect(validateImageInputType('https://example.com/image.png')).toBe(true)
-    expect(validateImageInputType('https://example.com/image.jpg')).toBe(true)
-    expect(validateImageInputType('https://example.com/image.svg')).toBe(true)
+    expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.png')).toBe(
+      true,
+    )
+    expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.jpg')).toBe(
+      true,
+    )
+    expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.svg')).toBe(
+      true,
+    )
   })
 
   it('should return false for invalid image URLs', () => {
-    expect(validateImageInputType('https://example.com/image.txt')).toBe(false)
-    expect(validateImageInputType('https://example.com/image')).toBe(false)
-    expect(validateImageInputType('https://example.com/notimage')).toBe(false)
+    expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.txt')).toBe(
+      false,
+    )
+    expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal')).toBe(
+      false,
+    )
+    expect(validateImageInputType('pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.png')).toBe(false)
   })
 })
