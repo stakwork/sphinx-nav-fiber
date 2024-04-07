02c16ae0f7302b08902f439e3cd0389bbaa4b14d
MahtabBukhari <mahtabali50447@gmail.com>
2024-04-07 06:44:53 +0500

    fix(image-for-any-node): fixed unit test

--- src/components/ModalsContainer/EditNodeNameModal/utils/__tests__/index.ts
+++ src/components/ModalsContainer/EditNodeNameModal/utils/__tests__/index.ts
@@ -5,9 +5,11 @@ describe('validateImageInputType function', () => {
     expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.png')).toBe(
       true,
     )
+
     expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.jpg')).toBe(
       true,
     )
+
     expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.svg')).toBe(
       true,
     )
@@ -17,9 +19,11 @@ describe('validateImageInputType function', () => {
     expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.txt')).toBe(
       false,
     )
+
     expect(validateImageInputType('https://pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal')).toBe(
       false,
     )
+
     expect(validateImageInputType('pbs.twimg.com/profile_images/1729542498006294529/AjwhArl6_normal.png')).toBe(false)
   })
 })
