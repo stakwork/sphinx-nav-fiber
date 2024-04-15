646a8aa4ff1c21a9084d602b2961a15dc5815296
AhsanFarooqDev <ahsanfarooqdevtech@gmail.com>
2024-04-15 22:38:02 +0500

    fix(valid-url): valid url at add content modal

--- src/components/AddContentModal/SourceStep/utils/index.ts
+++ src/components/AddContentModal/SourceStep/utils/index.ts
@@ -1,6 +1,6 @@
 // This regex is specifically for youtube videos, twitter spaces and mp3 links
 export const sourceUrlRegex =
-  /^(https?:\/\/)?(((?:[a-zA-Z\d](?:[a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-zA-Z\d%_.~+]*)*(\?[;&a-zA-Z\d%_.~+=-]*)?(#[-a-zA-Z\d_]*)?(@[a-zA-Z\d-]+)?$/i
+  /^(https:\/\/)(((?:[a-zA-Z\d](?:[a-zA-Z\d-]*[a-zA-Z\d])*)\.)+com|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-zA-Z\d%_.~+]*)*(\?[;&a-zA-Z\d%_.~+=-]*)?(#[-a-zA-Z\d_]*)?(@[a-zA-Z\d-]+)?$/i
 
 export function validateSourceURL(input: string) {
   if (sourceUrlRegex.test(input)) {
