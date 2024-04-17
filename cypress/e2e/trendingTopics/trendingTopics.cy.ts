3f6ce5adc060d2f9c35187c69a471865c949d8dc
Oluwatobi Bamidele <teebams49@gmail.com>
2024-04-18 00:18:39 +0100

    ci: use testing bitcoin as tredning topic

--- cypress/e2e/trendingTopics/trendingTopics.cy.ts
+++ cypress/e2e/trendingTopics/trendingTopics.cy.ts
@@ -2,6 +2,7 @@ describe('test trending topics', () => {
   it('Checking it trending topics exist', () => {
     cy.initialSetup('alice', 300)
     cy.get('.list').should('exist')
+    cy.contains('.list', '#Bitcoin').should('exist')
     cy.contains('.list', '#Misinformation').should('exist')
     cy.contains('.list', '#Billionaires').should('exist')
   })
