525217365623413af08274830627ad743014ca06
Oluwatobi Bamidele <teebams49@gmail.com>
2024-04-18 00:33:26 +0100

    test: remove Bitcoin from expected trending topic

--- cypress/e2e/trendingTopics/trendingTopics.cy.ts
+++ cypress/e2e/trendingTopics/trendingTopics.cy.ts
@@ -2,7 +2,6 @@ describe('test trending topics', () => {
   it('Checking it trending topics exist', () => {
     cy.initialSetup('alice', 300)
     cy.get('.list').should('exist')
-    cy.contains('.list', '#Bitcoin').should('exist')
     cy.contains('.list', '#Misinformation').should('exist')
     cy.contains('.list', '#Billionaires').should('exist')
   })
