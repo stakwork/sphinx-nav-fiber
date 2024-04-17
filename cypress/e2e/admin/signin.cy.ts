5dcd655240c0999ac826c16cc634a73bc116392a
MuhammadUmer44 <muhammadumerakhtar51@gmail.com>
2024-04-17 09:50:18 +0500

    fix(play-and-pause): fix cypress test

--- cypress/e2e/admin/signin.cy.ts
+++ cypress/e2e/admin/signin.cy.ts
@@ -7,7 +7,7 @@ describe('Admin Login', () => {
       url: 'http://localhost:8444/api/about*',
     }).as('updateAbout')
 
-    const title = `Testing NavFiber`
+    const title = `TestingNavFiber`
 
     // Open settings modal
     cy.get('div[data-testid="settings-modal"]').should('be.visible').click()
