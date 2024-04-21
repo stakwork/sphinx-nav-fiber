77b723f2b4e69ed6379a0f5704758d152f65e7b0
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2024-04-21 22:34:24 +0100

    feat: improved audio play/pause functionality and added tests

--- src/components/App/SideBar/Trending/BriefDescriptionModal/__tests__/index.tsx
+++ src/components/App/SideBar/Trending/BriefDescriptionModal/__tests__/index.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { BriefDescription } from '..'
 import { useAppStore } from '../../../../../../stores/useAppStore'
@@ -67,9 +67,10 @@ describe('BriefDescription Component Tests', () => {
   })
 
   it('ensures that listen Icon only display if the audio is not currently playing in the background', () => {
-    const { getByTestId } = render(<BriefDescription {...props} />)
+    const { getByTestId, getByText } = render(<BriefDescription {...props} />)
 
     expect(getByTestId('listen-icon')).toBeInTheDocument()
+    expect(getByText('Listen')).toBeInTheDocument()
   })
 
   it('ensures that pause icon only displays if the audio is playing in the background', () => {
@@ -77,9 +78,32 @@ describe('BriefDescription Component Tests', () => {
 
     useAppStoreMock.mockReturnValue({ currentPlayingAudio: mockCurrentPlayingAudio, setCurrentPlayingAudio: jest.fn() })
 
-    const { getByTestId } = render(<BriefDescription {...props} />)
+    const { getByTestId, getByText } = render(<BriefDescription {...props} />)
 
     expect(getByTestId('pause-icon')).toBeInTheDocument()
+    expect(getByText('Pause')).toBeInTheDocument()
+  })
+
+  it('ensures that clicking play in TLDR modal stops current background audio and starts new audio', async () => {
+    const setCurrentPlayingAudioMock = jest.fn()
+
+    useAppStoreMock.mockReturnValue({
+      currentPlayingAudio: { current: { src: 'random-audio-file', paused: false, pause: jest.fn() } },
+      setCurrentPlayingAudio: setCurrentPlayingAudioMock,
+    })
+
+    const { getByRole, container } = render(<BriefDescription {...props} />)
+
+    const listenButton = getByRole('button', { name: /Listen/i })
+
+    expect(listenButton).toBeInTheDocument()
+
+    fireEvent.click(listenButton)
+
+    waitFor(async () => {
+      expect(setCurrentPlayingAudioMock).toHaveBeenCalledWith(null)
+      expect(container.querySelector('audio')?.paused).toBe(true)
+    })
   })
 
   it('should call onClose when closing the modal', () => {
