698da1acf1b3c51121579cc4e33eda299fdb1a29
Расул <rasul.sahibjanli@gmail.com>
2024-04-22 12:13:14 +0300

    feat: add tests for calculateNodePositions

--- src/components/SettingsModal/SettingsView/GraphBlueprint/BlueprintModal/utils/index.ts
+++ src/components/SettingsModal/SettingsView/GraphBlueprint/BlueprintModal/utils/index.ts
@@ -10,43 +10,6 @@ export type SchemaExtended = SchemaWithChildren & {
   z: number
 }
 
-export const calculateNodePositionsCircular = (nodes: SchemaWithChildren[]): SchemaExtended[] => {
-  const root = nodes.find((i) => i.type === 'Thing')
-  const nodesWithPositions: SchemaExtended[] = []
-  const radius = 2
-
-  const loopThroughNodes = (
-    nodeId: string,
-    siblingsLength: number,
-    currentIndex: number,
-    parentAngle = 0,
-    depth = 0,
-  ) => {
-    const node = nodes.find((n) => n.ref_id === nodeId)
-
-    if (!node) {
-      return
-    }
-
-    const angle = siblingsLength ? (2 * Math.PI) / siblingsLength : 0
-    const angleStep = currentIndex * angle + parentAngle
-    const x = depth * radius * Math.cos(angleStep)
-    const y = depth * radius * Math.sin(angleStep)
-
-    nodesWithPositions.push({ ...node, x, y, z: 0 })
-
-    node.children.forEach((child, index) => {
-      loopThroughNodes(child, node.children.length, index, angleStep, depth + 1)
-    })
-  }
-
-  if (root?.ref_id) {
-    loopThroughNodes(root?.ref_id, 0, 0)
-  }
-
-  return nodesWithPositions
-}
-
 export const calculateNodePositions = (nodes: SchemaWithChildren[]): SchemaExtended[] => {
   const root = nodes.find((i) => i.type === 'Thing')
   const nodesWithPositions: SchemaExtended[] = []
