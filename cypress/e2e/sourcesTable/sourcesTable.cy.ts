f307870ebc7c1f153a3f6804b50e83b9c47660cb
Oluwatobi Bamidele <teebams49@gmail.com>
2024-04-22 21:41:25 +0100

    test: updated source table

--- cypress/e2e/sourcesTable/sourcesTable.cy.ts
+++ cypress/e2e/sourcesTable/sourcesTable.cy.ts
@@ -1,13 +1,49 @@
 describe('Sources Table / Home interactions', () => {
   it('Source Table Flow', () => {
-    cy.initialSetup('carol', 300)
+    let trCount
+
+    cy.initialSetup('alice', 300)
 
     cy.get('#cy-open-soure-table').click()
 
     cy.contains('button', 'Sources Table').click()
 
+    cy.get('tbody.MuiTableBody-root').find('tr').its('length').should('be.gt', 0)
+
     cy.contains('button', 'Twitter Handle').click()
 
-    cy.get('[data-testid="close-modal"]').click()
+    cy.get('tbody.MuiTableBody-root').find('tr').its('length').should('be.gt', 0)
+
+    cy.contains('button', 'Youtube channel').click()
+
+    cy.get('tbody.MuiTableBody-root').find('tr').its('length').should('be.gt', 0)
+
+    cy.contains('button', 'All').click()
+
+    cy.get('tbody.MuiTableBody-root')
+      .find('tr')
+      .its('length')
+      .then((count) => {
+        trCount = count
+
+        cy.get('tbody.MuiTableBody-root').find('tr').its('length').should('equal', trCount)
+
+        cy.get('tbody.MuiTableBody-root').find('tr').eq(0).find('.delete-wrapper').click()
+
+        cy.get('button[kind="small"]').contains('No').click()
+
+        cy.get('tbody.MuiTableBody-root').find('tr').its('length').should('equal', trCount)
+
+        cy.get('tbody.MuiTableBody-root').find('tr').eq(0).find('.delete-wrapper').click()
+
+        cy.get('button[kind="small"]').contains('Yes').click()
+
+        cy.get('tbody.MuiTableBody-root')
+          .find('tr')
+          .its('length')
+          .should('equal', trCount - 1)
+
+        cy.get('[data-testid="close-modal"]').click()
+      })
   })
 })
