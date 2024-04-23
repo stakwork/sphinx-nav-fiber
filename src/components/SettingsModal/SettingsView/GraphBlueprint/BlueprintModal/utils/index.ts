f8f61ec45a5fec649ec78a85d43e981628aad7a9
Расул <rasul.sahibjanli@gmail.com>
2024-04-23 12:55:11 +0300

    feat: fix tests

--- src/components/SettingsModal/SettingsView/GraphBlueprint/BlueprintModal/utils/index.ts
+++ src/components/SettingsModal/SettingsView/GraphBlueprint/BlueprintModal/utils/index.ts
@@ -24,20 +24,18 @@ export const calculateNodePositions = (nodes: SchemaWithChildren[]): SchemaExten
   ) => {
     const node = nodes.find((n) => n.ref_id === nodeId)
 
-    if (!node) {
-      return
+    if (node) {
+      const angle = siblingsLength ? (2 * Math.PI) / siblingsLength : 0
+      const angleStep = currentIndex * angle + parentAngle
+      const x = depth * radius * Math.cos(angleStep)
+      const y = depth * radius * Math.sin(angleStep)
+
+      nodesWithPositions.push({ ...node, x, y, z: 0 })
+
+      node.children.forEach((child, index) => {
+        loopThroughNodes(child, node.children.length, index, angleStep, depth + 1)
+      })
     }
-
-    const angle = siblingsLength ? (2 * Math.PI) / siblingsLength : 0
-    const angleStep = currentIndex * angle + parentAngle
-    const x = depth * radius * Math.cos(angleStep)
-    const y = depth * radius * Math.sin(angleStep)
-
-    nodesWithPositions.push({ ...node, x, y, z: 0 })
-
-    node.children.forEach((child, index) => {
-      loopThroughNodes(child, node.children.length, index, angleStep, depth + 1)
-    })
   }
 
   if (root?.ref_id) {
