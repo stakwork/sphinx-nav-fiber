85dd75a76d4f5e11f6f6d5a0a0401d1731f4ab26
Расул <rasul.sahibjanli@gmail.com>
2024-05-08 15:28:17 +0300

    feat: fix lint

--- src/components/SourcesTableModal/SourcesView/Topics/MergeTopicModal/__tests__/index.tsx
+++ src/components/SourcesTableModal/SourcesView/Topics/MergeTopicModal/__tests__/index.tsx
@@ -1,8 +1,8 @@
-import { render, fireEvent, waitFor } from '@testing-library/react'
-import { MergeTopicModal } from '../index'
-import * as TopicsStore from '~/stores/useTopicsStore'
 import '@testing-library/jest-dom'
+import { fireEvent, render, waitFor } from '@testing-library/react'
 import { postMergeTopics } from '~/network/fetchSourcesData'
+import * as TopicsStore from '~/stores/useTopicsStore'
+import { MergeTopicModal } from '../index'
 
 jest.mock('~/stores/useTopicsStore')
 jest.mock('~/network/fetchSourcesData')
@@ -18,6 +18,8 @@ describe('MergeTopicModal', () => {
       total: 3,
       setState: mockSetState,
     }))
+
+    //
     ;(postMergeTopics as jest.Mock).mockImplementation(mockPostMergeTopics)
 
     const { getByTestId } = render(
