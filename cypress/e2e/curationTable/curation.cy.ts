b7c4ea703050ebb816fca711fbda5b75ab4bab27
Oluwatobi Bamidele <teebams49@gmail.com>
2024-05-21 18:04:59 +0100

    test: unmute topics

--- cypress/e2e/curationTable/curation.cy.ts
+++ cypress/e2e/curationTable/curation.cy.ts
@@ -141,7 +141,7 @@ describe('Test Curation Table', () => {
       })
   })
 
-  it('Mute Topic', () => {
+  it.skip('Mute Topic', () => {
     cy.intercept({
       method: 'GET',
       url: 'http://localhost:8444/api/nodes/info?skip=0&limit=50&muted=False&sort_by=date&node_type=Topic*',
@@ -214,6 +214,103 @@ describe('Test Curation Table', () => {
     })
 
     cy.get('div[data-testid="mute"]').click()
+
+    cy.wait('@muteTopic')
+
+    cy.get('thead').contains('Muted').click()
+
+    cy.get('tbody')
+      .find('tr')
+      .each(($tr) => {
+        cy.wrap($tr)
+          .find('td:nth-child(2)')
+          .invoke('text')
+          .then((text) => {
+            if (text.trim() === secondSpecificValue) {
+              secondMatchFound = true
+            }
+          })
+      })
+      .then(() => {
+        expect(secondMatchFound).to.be.true
+      })
+  })
+
+  it('Unmute Topic', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:8444/api/nodes/info?skip=0&limit=50&muted=False&sort_by=date&node_type=Topic*',
+    }).as('loadTopics')
+
+    cy.intercept({
+      method: 'PUT',
+      url: 'http://localhost:8444/api/node*',
+    }).as('muteTopic')
+
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:8444/api/nodes/info?skip=0&limit=50&muted=True&sort_by=date&node_type=Topic*',
+    }).as('loadMutedTopics')
+
+    let specificValue = ''
+    let secondSpecificValue = ''
+    let firstMatchFound = false
+    let secondMatchFound = false
+
+    cy.initialSetup('alice', 300)
+
+    cy.get('#cy-open-soure-table').click()
+
+    cy.get('[data-testid="sources-table"]').should('exist')
+
+    cy.contains('button', 'Topics').click()
+
+    cy.wait('@loadTopics')
+
+    cy.get('thead').contains('Muted').click()
+
+    cy.get('tbody > tr:first').within(() => {
+      cy.get('td:nth-child(2)').then(($td) => {
+        // Access the value of the first td
+        const tdValue = $td.text().trim()
+        specificValue = tdValue
+        cy.get('.approve-wrapper button').eq(0).click()
+      })
+    })
+
+    cy.wait('@muteTopic')
+
+    cy.get('thead').contains('Muted').click()
+
+    cy.wait('@loadTopics')
+
+    cy.get('tbody')
+      .find('tr')
+      .each(($tr) => {
+        cy.wrap($tr)
+          .find('td:nth-child(2)')
+          .invoke('text')
+          .then((text) => {
+            if (text.trim() === specificValue) {
+              firstMatchFound = true
+            }
+          })
+      })
+      .then(() => {
+        expect(firstMatchFound).to.be.true
+      })
+
+    cy.get('thead').contains('Muted').click()
+
+    cy.get('tbody > tr:first').within(() => {
+      cy.get('td:nth-child(2)').then(($td) => {
+        const tdValue = $td.text().trim()
+        secondSpecificValue = tdValue
+        cy.get('.approve-wrapper button').eq(1).click()
+      })
+    })
+
+    cy.get('div[data-testid="unmute"]').click()
 
     cy.wait('@muteTopic')
 
