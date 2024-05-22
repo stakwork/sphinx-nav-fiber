79a8018142d5ea8d7f773c1ed5b9cf933da302ac
aliraza556 <alirazaakhtar1122@gmail.com>
2024-05-22 22:01:53 +0500

    fix(sidebarSubView): fix cypress test

--- cypress/e2e/seeLatest/latest.cy.ts
+++ cypress/e2e/seeLatest/latest.cy.ts
@@ -5,7 +5,7 @@ describe('See latest button as new node are added', () => {
     // add tweet node
     cy.fixture('trendingTopics.json').then(async (data) => {
       //   await addTweetNodeToJarvisBackend(data)
-      for (let i = 0; i < data.length; i+=1) {
+      for (let i = 0; i < data.length; i += 1) {
         let newDate
         const date = new Date()
 
@@ -54,7 +54,9 @@ describe('See latest button as new node are added', () => {
 
       cy.get('[data-testid="close-sidebar-sub-view"]').click()
 
-      cy.get('[data-testid="sidebar-sub-view"]').should('have.css', 'position', 'absolute')
+      cy.get('[data-testid="sidebar-sub-view"]').should(($el) => {
+        expect($el).to.have.css('position', 'absolute')
+      })
     })
   })
 })
