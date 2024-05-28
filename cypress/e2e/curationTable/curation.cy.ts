0bfcaa09e9d1e572b01c366c6a7f276da29d5dbf
Oluwatobi Bamidele <teebams49@gmail.com>
2024-05-28 20:45:04 +0100

    fix: fixed how we type into the topic input and click on merge button

--- cypress/e2e/curationTable/curation.cy.ts
+++ cypress/e2e/curationTable/curation.cy.ts
@@ -119,12 +119,12 @@ describe('Test Curation Table', () => {
     })
     cy.get('div[data-testid="merge"]').click()
 
-    cy.get('[data-testid="to-section-container"]').find('input[type="text"]').type(mergeTopic)
+    cy.get('#blur-on-select').type(mergeTopic)
 
     cy.wait('@topicSearch')
     cy.get('div[data-testid="authenticity"]').click()
 
-    cy.get(':nth-child(10) > #mergeTopic > [data-testid="merge-topics-button"]').click()
+    cy.get('[data-testid="merge-topics-button"]').click()
 
     cy.wait('@mergeTopic')
 
