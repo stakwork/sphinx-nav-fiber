c9d25224a1f7032afde0771b3e937ef2b8dd7a91
Shoaibdev7 <shoaibdev7@gmail.com>
2024-06-13 06:01:16 +0500

    fix(tldr): unable to take a screenshot on mac

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -134,12 +134,23 @@ export const BaseModal = ({
       }
     }
 
+    const handleSpecialKeys = (e: KeyboardEvent) => {
+      const keyCombination = e.metaKey && e.ctrlKey && (e.key === '4' || e.key === '3')
+
+      if (keyCombination) {
+        e.stopPropagation()
+        e.preventDefault()
+      }
+    }
+
     if (visible) {
       document.addEventListener('keydown', handleEsc)
+      document.addEventListener('keydown', handleSpecialKeys)
     }
 
     return () => {
-      document.removeEventListener('keydown', handleEsc) // Cleanup
+      document.removeEventListener('keydown', handleEsc)
+      document.removeEventListener('keydown', handleSpecialKeys)
     }
   }, [visible, close])
 
