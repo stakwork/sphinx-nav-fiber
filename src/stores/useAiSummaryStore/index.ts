63c86a598143e8194c83d0cd0bea88c6470c3b57
Oluwatobi Bamidele <teebams49@gmail.com>
2024-07-05 17:27:47 +0100

    feat: ai_summary_store

---
+++ src/stores/useAiSummaryStore/index.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand'
+
+export type AiSummaryStore = {
+  aiSummaryIsLoading: boolean
+  aiSummaryAnswers: { [k: string]: string }
+  setAiSummaryAnswer: (key: string, answer: string) => void
+  setAiSummaryIsLoading: (status: boolean) => void
+  getAiSummaryAnswer: (key: string) => string
+}
+
+const defaultData = {
+  aiSummaryIsLoading: false,
+  aiSummaryAnswers: {},
+}
+
+export const useAppStore = create<AiSummaryStore>((set, get) => ({
+  ...defaultData,
+  setAiSummaryIsLoading: (status) => set({ aiSummaryIsLoading: status }),
+  setAiSummaryAnswer: (key, answer) => {
+    const summaryAnswers = get().aiSummaryAnswers
+    const newSummaryAnswers = { ...summaryAnswers, [key]: answer }
+    set({ aiSummaryAnswers: { ...newSummaryAnswers } })
+  },
+  getAiSummaryAnswer: (key) => {
+    const summaryAnswers = get().aiSummaryAnswers
+    return summaryAnswers[key]
+  },
+}))
