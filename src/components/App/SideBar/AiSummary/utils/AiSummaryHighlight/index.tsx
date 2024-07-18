391ea2e34f3f6b90da6155dae72e45d678ceba38
AbdulWahab3181 <abdulwahab3181@gmail.com>
2024-07-18 21:35:33 +0500

    feat(input): implemented highlighted tooltip

--- src/components/App/SideBar/AiSummary/utils/AiSummaryHighlight/index.tsx
+++ src/components/App/SideBar/AiSummary/utils/AiSummaryHighlight/index.tsx
@@ -1,6 +1,13 @@
+import React from 'react'
 import styled from 'styled-components'
 import { colors } from '~/utils'
 import { ExtractedEntity } from '~/types/index'
+
+type HighlightProps = {
+  part: string
+  matchedEntity: ExtractedEntity | undefined
+  handleSubmit: (search: string) => void
+}
 
 export function highlightAiSummary(
   sDescription: string,
@@ -8,7 +15,7 @@ export function highlightAiSummary(
   entities?: ExtractedEntity[],
 ) {
   if (!entities || entities.length === 0) {
-    return sDescription
+    return <>{sDescription}</>
   }
 
   const sortedEntities = entities
@@ -20,27 +27,25 @@ export function highlightAiSummary(
   const regex = new RegExp(`(${escapedTerms.join('|')})`, 'gi')
 
   const parts = sDescription.split(regex)
-  const highlighted = new Set()
+
+  let keyIndex = 0
 
   return (
     <>
       {parts.map((part) => {
-        if (regex.test(part) && !highlighted.has(part.toLowerCase())) {
-          highlighted.add(part.toLowerCase())
+        const matchedEntity = entities.find((entity) => entity.entity.toLowerCase() === part.toLowerCase())
 
-          return (
-            <Highlight
-              key={part}
-              onClick={() => {
-                handleSubmit(part)
-              }}
-            >
-              {part}
-            </Highlight>
-          )
+        if (regex.test(part) && matchedEntity) {
+          const highlightKey = `${matchedEntity.entity}-highlight-${keyIndex}`
+
+          keyIndex += 1
+
+          return <Highlight key={highlightKey} handleSubmit={handleSubmit} matchedEntity={matchedEntity} part={part} />
         }
 
-        return part
+        keyIndex += 1
+
+        return <React.Fragment key={`fragment-${keyIndex}`}>{part}</React.Fragment>
       })}
     </>
   )
@@ -50,13 +55,49 @@ function escapeRegExp(string: string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 }
 
-const Highlight = styled.span`
+const Highlight = ({ handleSubmit, matchedEntity, part }: HighlightProps) => {
+  const [showTooltip, setShowTooltip] = React.useState(false)
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true)
+  }
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false)
+  }
+
+  return (
+    <HighlightWrapper
+      onClick={() => handleSubmit(part)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {part}
+      {showTooltip && matchedEntity && <Tooltip>{matchedEntity.description}</Tooltip>}
+    </HighlightWrapper>
+  )
+}
+
+const HighlightWrapper = styled.span`
   padding: 0;
   margin: 0;
   color: ${colors.SECONDARY_BLUE};
+  cursor: pointer;
+  position: relative;
 
   &:hover {
     text-decoration: underline;
-    cursor: pointer;
   }
+`
+
+const Tooltip = styled.span`
+  position: absolute;
+  background-color: white;
+  color: black;
+  padding: 0.5rem;
+  border-radius: 4px;
+  font-size: 12px;
+  white-space: normal;
+  z-index: 10000;
+  visibility: 'visible';
 `
