bfd82226b2cba1dd154f6fb5a6d74cab21d10428
saithsab877 <saithsab877@gmail.com>
2024-08-05 18:34:33 +0500

    fix(add-content): all cy test

--- cypress/e2e/addContent/addYoutube.cy.ts
+++ cypress/e2e/addContent/addYoutube.cy.ts
@@ -2,17 +2,23 @@ describe('Add Youtube Content', () => {
   it('Carol adds youtube video to graph', () => {
     cy.initialSetup('carol', 50)
 
-    cy.intercept({
-      method: 'POST',
-      url: 'http://localhost:8444/api/add_node*',
-    }).as('addYoutube')
+    cy.intercept(
+      {
+        method: 'POST',
+        url: 'http://localhost:8444/api/add_node*',
+      },
+      {
+        statusCode: 200,
+        body: { success: true },
+      },
+    ).as('addYoutube')
 
     cy.get('[data-testid="add-content-modal"]').click()
     cy.get('#addContent').should('exist')
     cy.get('[id="cy-youtube-channel-id"]').type('https://www.youtube.com/watch?v=dPLPSaFqJmY')
     cy.wait(1000)
     cy.get('[data-testid="add-content-btn"]').should('not.be.disabled').click()
-    cy.get('[data-testid="skip-location-btn"').click()
+    cy.get('[data-testid="skip-location-btn"]').click()
     cy.get('[data-testid="check-icon"]').click()
 
     cy.wait('@addYoutube')
