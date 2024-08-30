9c41d31a96b9724b94d9e0f3c548bd8336aa08cb
Oluwatobi Bamidele <teebams49@gmail.com>
2024-08-31 00:23:11 +0100

    refactor: filter topics in a new test file

--- cypress/e2e/curationTable/filterTopics.ts
+++ cypress/e2e/curationTable/filterTopics.ts
@@ -1,5 +1,5 @@
-describe('Test Curation Table', () => {
-  it.skip('Filter topics', () => {
+describe('Filter', () => {
+  it('Filter topics', () => {
     cy.intercept({
       method: 'GET',
       url: 'http://localhost:8444/api/nodes/info?skip=0&limit=50&muted=False&sort_by=date&node_type=Topic*',
