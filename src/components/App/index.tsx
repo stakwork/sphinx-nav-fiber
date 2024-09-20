2fea877af745227ab589624284c473cb224258f3
Shoaibdev7 <shoaibdev7@gmail.com>
2024-09-20 12:56:35 +0500

    fix(ai-chat): ai screen on default part of feature flag

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -238,10 +238,10 @@ export const App = () => {
   ])
 
   useEffect(() => {
-    if (!splashDataLoading) {
+    if (chatInterfaceFeatureFlag && !splashDataLoading) {
       setUniverseQuestionIsOpen()
     }
-  }, [setUniverseQuestionIsOpen, splashDataLoading])
+  }, [setUniverseQuestionIsOpen, splashDataLoading, chatInterfaceFeatureFlag])
 
   return (
     <>
@@ -252,7 +252,7 @@ export const App = () => {
       <Leva hidden={!isDevelopment} />
 
       <Suspense fallback={<div>Loading...</div>}>
-        {!splashDataLoading ? (
+        {!splashDataLoading && chatInterfaceFeatureFlag ? (
           <Wrapper direction="row">
             <FormProvider {...form}>
               <LazyMainToolbar />
