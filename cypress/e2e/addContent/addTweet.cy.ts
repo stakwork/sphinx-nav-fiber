bb2372ba8d95ce78236ca74a80fd00ca667e7185
Oluwatobi Bamidele <teebams49@gmail.com>
2024-10-01 14:40:41 +0100

    fix: log response from adding tweet

--- cypress/e2e/addContent/addTweet.cy.ts
+++ cypress/e2e/addContent/addTweet.cy.ts
@@ -24,7 +24,9 @@ describe('Add Tweet Content', () => {
       url: 'http://localhost:8444/api/add_node*',
     }).as('addTweet2')
 
-    cy.wait('@addTweet2') // This is because add source is currently skipped,
+    cy.wait('@addTweet2').then((intersection) => {
+      cy.log(intersection.response)
+    }) // This is because add source is currently skipped,
 
     cy.get('.Toastify__toast-body').should('have.text', 'Content Added')
 
