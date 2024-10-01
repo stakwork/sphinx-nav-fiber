bcec956511eb15d235689ac848a491449416ee77
Расул <rasul.sahibjanli@gmail.com>
2024-10-01 15:06:26 +0300

    feat: added code formater for selected view

--- src/utils/apiUrlFromSwarmHost/index.ts
+++ src/utils/apiUrlFromSwarmHost/index.ts
@@ -22,7 +22,7 @@ export function apiUrlFromSwarmHost(): string | undefined {
       url = `https://${finalHost}`
     }
   } else if (origin.includes('localhost')) {
-    url = 'https://bitcoin.sphinx.chat'
+    url = 'https://boltwall.swarm38.sphinx.chat'
   }
 
   return `${url}/api`
