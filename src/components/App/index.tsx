cf9d15df7a523d68fe793404dfc739c12f2b1100
Shoaibdev7 <shoaibdev7@gmail.com>
2024-10-18 12:37:57 +0500

    fix(ai-chat): ai screen on default part of feature flag

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -340,7 +340,7 @@ export const App = () => {
           <Wrapper direction="row">
             <FormProvider {...form}>
               <LazyMainToolbar />
-              {!universeQuestionIsOpen && <LazySideBar />}
+              {!universeQuestionIsOpen && chatInterfaceFeatureFlag && <LazySideBar />}
               <LazyUniverse />
               <Overlay />
               <AppBar />
