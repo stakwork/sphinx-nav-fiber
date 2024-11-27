a97fefb274f2be52326fd06bca8919271d8f7cb8
MahtabBukhari <mahtabali50447@gmail.com>
2024-11-27 22:04:01 +0500

    fix(seed-questions): add seed episodes logic to landing page

--- src/components/mindset/components/LandingPage/index.tsx
+++ src/components/mindset/components/LandingPage/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FieldValues } from 'react-hook-form'
 import styled from 'styled-components'
 import { Flex } from '~/components/common/Flex'
@@ -6,10 +6,13 @@ import { NODE_ADD_ERROR } from '~/constants'
 import { api } from '~/network/api'
 import { useDataStore } from '~/stores/useDataStore'
 import { useMindsetStore } from '~/stores/useMindsetStore'
-import { SubmitErrRes } from '~/types'
+import { NodeExtended, SubmitErrRes } from '~/types'
 import { colors } from '~/utils/colors'
 import { ChevronRight } from '../Icon/ChevronRight'
 import { isValidMediaUrl } from './utils'
+import ReactPlayer from 'react-player'
+import { getAboutData, getNode } from '~/network/fetchSourcesData'
+import { Avatar } from '~/components/common/Avatar'
 
 export type FormData = {
   input: string
@@ -18,6 +21,12 @@ export type FormData = {
   longitude: string
   latitude: string
 }
+
+const Media = ({ mediaLink }: { mediaLink: string }) => (
+  <PlayerWrapper>
+    <ReactPlayer controls height="121px" url={`${mediaLink}`} width="173px" />
+  </PlayerWrapper>
+)
 
 const handleSubmitForm = async (data: FieldValues): Promise<SubmitErrRes> => {
   const endPoint = 'add_node'
@@ -40,10 +49,31 @@ const handleSubmitForm = async (data: FieldValues): Promise<SubmitErrRes> => {
 
 export const LandingPage = () => {
   const [inputValue, setInputValue] = useState('')
+  const [nodeData, setNodeData] = useState<NodeExtended[]>([])
   const [error, setError] = useState(false)
   const [requestError, setRequestError] = useState<string>('')
   const { setRunningProjectId } = useDataStore((s) => s)
   const { setSelectedEpisodeId, setSelectedEpisodeLink } = useMindsetStore((s) => s)
+
+  useEffect(() => {
+    const run = async () => {
+      const response = await getAboutData()
+
+      if (!response?.seed_questions) {
+        setNodeData([])
+
+        return
+      }
+
+      const refIds = response?.seed_questions.slice(0, 3)
+
+      const nodeResponses = await Promise.all(refIds.map((id: string) => getNode(id)))
+
+      setNodeData(nodeResponses)
+    }
+
+    run()
+  }, [])
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
@@ -104,6 +134,19 @@ export const LandingPage = () => {
         </IconWrapper>
       </InputWrapper>
       {requestError && <div>{requestError}</div>}
+      <SeedQuestionsWrapper>
+        {nodeData.map((node) => (
+          <SeedQuestion key={node?.properties?.episode_title}>
+            {node?.properties?.source_link && <Media mediaLink={node?.properties?.source_link} />}
+            {!node?.properties?.source_link && node?.properties?.image_url && (
+              <Cover>
+                <Avatar size={120} src={node?.properties?.image_url || ''} type="clip" />
+              </Cover>
+            )}
+            {node?.properties?.episode_title && <EpisodeTitle>{node?.properties?.episode_title}</EpisodeTitle>}
+          </SeedQuestion>
+        ))}
+      </SeedQuestionsWrapper>
     </Wrapper>
   )
 }
@@ -137,7 +180,7 @@ const Title = styled(Flex)`
 
 const Input = styled.input<{ error?: boolean }>`
   width: 100%;
-  max-width: 450px;
+  max-width: 648px;
   padding: 12px 28px 12px 16px;
   border-radius: 100px;
   border: 1px solid ${(props) => (props.error ? 'red' : colors.DIVIDER_4)};
@@ -158,7 +201,7 @@ const Input = styled.input<{ error?: boolean }>`
 
 const InputWrapper = styled.div`
   position: relative;
-  width: 450px;
+  width: 648px;
   display: flex;
   align-items: center;
 `
@@ -177,4 +220,61 @@ const IconWrapper = styled.div<{ error?: boolean }>`
     height: 17px;
     color: ${colors.GRAY6};
   }
+`
+
+const SeedQuestionsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  margin-top: 20px;
+  max-width: 648px;
+  height: 237px;
+`
+
+const SeedQuestion = styled.div`
+  background: ${colors.BG1};
+  width: 205px;
+  height: 200px;
+  color: ${colors.white};
+  padding: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  align-items: start;
+  flex-direction: column;
+  text-align: left;
+  &:hover {
+    background: ${colors.SEEDQUESTION_HOVER};
+  }
+
+  &:active {
+    background: ${colors.SEEDQUESTION};
+  }
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+
+  path {
+    fill: ${colors.modalWhiteOverlayBg};
+  }
+`
+
+const PlayerWrapper = styled.div`
+  width: 100%;
+  cursor: pointer;
+`
+
+const EpisodeTitle = styled.p`
+  font-family: Inter;
+  font-weight: 500;
+  font-size: 16px;
+`
+
+const Cover = styled(Flex)`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `
