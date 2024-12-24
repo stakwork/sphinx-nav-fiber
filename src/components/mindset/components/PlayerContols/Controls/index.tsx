531af675bc667a21d64400b65b888f2dcfc58a87
MahtabBukhari <mahtabali50447@gmail.com>
2024-12-24 13:48:09 +0500

    fix(add-buttons): add mute and playback speed button

--- src/components/mindset/components/PlayerContols/Controls/index.tsx
+++ src/components/mindset/components/PlayerContols/Controls/index.tsx
@@ -1,16 +1,23 @@
 import { IconButton } from '@mui/material'
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import styled from 'styled-components'
 import PauseIcon from '~/components/Icons/PauseIcon'
 import PlayIcon from '~/components/Icons/PlayIcon'
 import { Flex } from '~/components/common/Flex'
 import { usePlayerStore } from '~/stores/usePlayerStore'
 import { colors } from '~/utils/colors'
+import VolumeIcon from '~/components/Icons/VolumeIcon'
+import MuteVolumeIcon from '~/components/Icons/MuteVolumeIcon'
 
 export const Controls = memo(() => {
   const isPlaying = usePlayerStore((s) => s.isPlaying)
   const setIsPlaying = usePlayerStore((s) => s.setIsPlaying)
+  const setVolume = usePlayerStore((s) => s.setVolume)
+  const setPlaybackSpeed = usePlayerStore((s) => s.setPlaybackSpeed)
+  const playbackSpeed = usePlayerStore((s) => s.playbackSpeed)
   const playerRef = usePlayerStore((s) => s.playerRef)
+  const [isMuted, setIsMuted] = useState(false)
+  const speedOptions = [0.5, 1, 1.5, 2]
 
   const handleRewind = () => {
     if (playerRef) {
@@ -32,8 +39,30 @@ export const Controls = memo(() => {
     setIsPlaying(!isPlaying)
   }
 
+  const toggleMute = () => {
+    setIsMuted(!isMuted)
+    setVolume(isMuted ? 1 : 0)
+  }
+
+  const cycleSpeed = () => {
+    const currentIndex = speedOptions.indexOf(playbackSpeed)
+    const nextIndex = (currentIndex + 1) % speedOptions.length
+    const newSpeed = speedOptions[nextIndex]
+
+    setPlaybackSpeed(newSpeed)
+
+    if (playerRef) {
+      const internalPlayer = playerRef.getInternalPlayer() as HTMLMediaElement
+
+      if (internalPlayer && typeof internalPlayer.playbackRate !== 'undefined') {
+        internalPlayer.playbackRate = newSpeed
+      }
+    }
+  }
+
   return (
     <Wrapper>
+      <MuteWrapper onClick={toggleMute}>{isMuted ? <MuteVolumeIcon /> : <VolumeIcon />}</MuteWrapper>
       <RewindIconWrapper onClick={handleRewind}>
         <img alt="" src="RewindIcon.svg" />
       </RewindIconWrapper>
@@ -43,6 +72,7 @@ export const Controls = memo(() => {
       <ForwardIconWrapper onClick={handleFastForward}>
         <img alt="" src="ForwardIcon.svg" />
       </ForwardIconWrapper>
+      <SpeedButton onClick={cycleSpeed}>{playbackSpeed}x</SpeedButton>
     </Wrapper>
   )
 })
@@ -54,7 +84,7 @@ const Wrapper = styled(Flex).attrs({
   align: 'center',
   justify: 'flex-start',
 })`
-  width: 142px;
+  width: 200px;
   height: 54px;
   background: ${colors.BG1};
   border-radius: 40px;
@@ -95,4 +125,39 @@ const ForwardIconWrapper = styled.div`
     width: 24px;
     height: auto;
   }
+`
+
+const MuteWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  border-radius: 50%;
+  transition: background-color 0.3s ease;
+  color: ${colors.GRAY6};
+
+  svg {
+    margin-top: 3px;
+    width: 24px;
+    height: 24px;
+  }
+
+  &:hover {
+    background-color: ${colors.DROPDOWN_BG};
+  }
+`
+
+const SpeedButton = styled.button`
+  margin-top: 4px;
+  background: ${colors.BG1};
+  color: ${colors.GRAY6};
+  border: none;
+  width: 8px;
+  border-radius: 20px;
+  padding: 8px 20px 8px 8px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
 `
