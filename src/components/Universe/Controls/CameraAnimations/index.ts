0474fb0f8c65effaa2031b7731c80c64b3e7cfa5
Расул <rasul.sahibjanli@gmail.com>
2024-12-31 18:34:50 +0300

    feat: remove rotation

--- src/components/Universe/Controls/CameraAnimations/index.ts
+++ src/components/Universe/Controls/CameraAnimations/index.ts
@@ -1,16 +1,11 @@
 /* eslint-disable no-param-reassign */
 import type { CameraControls } from '@react-three/drei'
-import { MathUtils } from 'three'
 
-import { useFrame } from '@react-three/fiber'
 import { RefObject, useEffect } from 'react'
 
-import { useControlStore } from '~/stores/useControlStore'
 import { useGraphStore, useSelectedNode } from '~/stores/useGraphStore'
 import { initialCameraPosition } from './constants'
 import { useAutoNavigate } from './useAutoNavigate'
-
-const autoRotateSpeed = 1
 
 let cameraAnimation: gsap.core.Tween | null = null
 
@@ -45,20 +40,6 @@ export const useCameraAnimations = (
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedNode, graphRadius])
-
-  useFrame((_, delta) => {
-    if (cameraControlsRef.current) {
-      //  do camera rotation
-      const { disableCameraRotation } = useGraphStore.getState()
-      const { isUserDragging } = useControlStore.getState()
-
-      if (!disableCameraRotation && !isUserDragging && false) {
-        cameraControlsRef.current.azimuthAngle += autoRotateSpeed * delta * MathUtils.DEG2RAD
-      }
-
-      cameraControlsRef.current.update(delta)
-    }
-  })
 
   return null
 }
