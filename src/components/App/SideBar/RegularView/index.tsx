7271703163493ee9566c492c4c2e2847c6acb4a7
jordan-ae <arreyetta1jnr@gmail.com>
2025-02-07 15:07:14 +0100

    fix: remove blue loader

--- src/components/App/SideBar/RegularView/index.tsx
+++ src/components/App/SideBar/RegularView/index.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 import React, { useEffect, useRef, useState } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { ClipLoader } from 'react-spinners'
+import { BeatLoader, ClipLoader } from 'react-spinners'
 import styled from 'styled-components'
 import { SelectWithPopover } from '~/components/App/SideBar/Dropdown'
 import { FilterSearch } from '~/components/App/SideBar/FilterSearch'
@@ -12,7 +12,6 @@ import SearchFilterIcon from '~/components/Icons/SearchFilterIcon'
 import SearchIcon from '~/components/Icons/SearchIcon'
 import { SearchBar } from '~/components/SearchBar'
 import { Flex } from '~/components/common/Flex'
-import { FetchLoaderText } from '~/components/common/Loader'
 import { useAppStore } from '~/stores/useAppStore'
 import { useDataStore, useFilteredNodes } from '~/stores/useDataStore'
 import { useFeatureFlagStore } from '~/stores/useFeatureFlagStore'
@@ -126,7 +125,9 @@ export const RegularView = () => {
         {searchTerm && (
           <SearchDetails>
             {isLoading ? (
-              <FetchLoaderText />
+              <SearchLoader>
+                Searching <BeatLoader color={colors.SECONDARY_BLUE} size={2} />
+              </SearchLoader>
             ) : (
               <>
                 <div className="left">
@@ -265,4 +266,10 @@ const IconWrapper = styled.div<{ isFilterOpen: boolean }>`
     color: ${({ isFilterOpen }) => (isFilterOpen ? colors.black : colors.GRAY7)};
     fill: none;
   }
+`
+
+const SearchLoader = styled(Flex)`
+  display: flex;
+  align-items: baseline;
+  flex-direction: row;
 `
