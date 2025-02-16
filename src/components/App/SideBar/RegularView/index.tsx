b8664323194b26ebf4b1046d611032c5eb53bbb0
jordan-ae <arreyetta1jnr@gmail.com>
2025-02-16 14:27:16 +0100

    fix: fix merge conflicts

--- src/components/App/SideBar/RegularView/index.tsx
+++ src/components/App/SideBar/RegularView/index.tsx
@@ -1,18 +1,9 @@
 import { useRef } from 'react'
 import styled from 'styled-components'
-import { SelectWithPopover } from '~/components/App/SideBar/Dropdown'
-import { FilterSearch } from '~/components/App/SideBar/FilterSearch'
-import ClearIcon from '~/components/Icons/ClearIcon'
-import SearchFilterCloseIcon from '~/components/Icons/SearchFilterCloseIcon'
-import SearchFilterIcon from '~/components/Icons/SearchFilterIcon'
-import SearchIcon from '~/components/Icons/SearchIcon'
-import { SearchBar } from '~/components/SearchBar'
-import { useNodeNavigation } from '~/components/Universe/useNodeNavigation'
 import { Flex } from '~/components/common/Flex'
 import { useAppStore } from '~/stores/useAppStore'
 import { useDataStore } from '~/stores/useDataStore'
 import { useFeatureFlagStore } from '~/stores/useFeatureFlagStore'
-import { colors } from '~/utils/colors'
 import { LatestView } from '../Latest'
 import { Relevance } from '../Relevance'
 import { EpisodeSkeleton } from '../Relevance/EpisodeSkeleton'
@@ -21,11 +12,7 @@ import { Trending } from '../Trending'
 export const MENU_WIDTH = 390
 
 export const RegularView = () => {
-  const { isFetching: isLoading, setSidebarFilter } = useDataStore((s) => s)
-
-  const { navigateToNode } = useNodeNavigation()
-
-  const filteredNodes = useFilteredNodes()
+  const { isFetching: isLoading } = useDataStore((s) => s)
 
   const { currentSearch: searchTerm } = useAppStore((s) => s)
 
@@ -34,75 +21,15 @@ export const RegularView = () => {
   const componentRef = useRef<HTMLDivElement | null>(null)
 
   return (
-    <>
-      <SearchWrapper className={clsx({ 'has-shadow': isScrolled })}>
-        <SearchFilterIconWrapper>
-          <Search>
-            <SearchBar />
-            <InputButton
-              data-testid="search_action_icon"
-              onClick={() => {
-                if (searchTerm) {
-                  setValue('search', '')
-                  clearSearch()
-                  setSidebarFilter('all')
-                  navigateToNode(null)
-                  navigate(`/`)
-
-                  return
-                }
-
-                if (typing.trim() === '') {
-                  return
-                }
-
-                const encodedQuery = typing.replace(/\s+/g, '+')
-
-                navigate(`/search?q=${encodedQuery}`)
-              }}
-            >
-              {!isLoading ? (
-                <>{searchTerm?.trim() ? <ClearIcon /> : <SearchIcon />}</>
-              ) : (
-                <ClipLoader color={colors.SECONDARY_BLUE} data-testid="loader" size="20" />
-              )}
-            </InputButton>
-          </Search>
-
-          <IconWrapper data-testid="search_filter_icon" isFilterOpen={isFilterOpen} onClick={handleFilterIconClick}>
-            {isFilterOpen ? <SearchFilterCloseIcon /> : <SearchFilterIcon />}
-          </IconWrapper>
-
-          <FilterSearch anchorEl={anchorEl} onClose={handleCloseFilterSearch} setAnchorEl={setAnchorEl} />
-        </SearchFilterIconWrapper>
-        {searchTerm && (
-          <SearchDetails>
-            {isLoading ? (
-              <FetchLoaderText />
-            ) : (
-              <>
-                <div className="left">
-                  <span className="count">{filteredNodes.length}</span>
-                  <span className="label"> results</span>
-                </div>
-                <div className="right" style={{ alignItems: 'center' }}>
-                  <SelectWithPopover />
-                </div>
-              </>
-            )}
-          </SearchDetails>
-        )}
-      </SearchWrapper>
-      <ScrollWrapper ref={componentRef}>
-        {!searchTerm && trendingTopicsFeatureFlag && (
-          <TrendingWrapper>
-            <Trending />
-          </TrendingWrapper>
-        )}
-        {!searchTerm && <LatestView />}
-        {isLoading ? <EpisodeSkeleton /> : <Relevance isSearchResult={!!searchTerm} />}
-      </ScrollWrapper>
-    </>
+    <ScrollWrapper ref={componentRef}>
+      {!searchTerm && trendingTopicsFeatureFlag && (
+        <TrendingWrapper>
+          <Trending />
+        </TrendingWrapper>
+      )}
+      {!searchTerm && <LatestView />}
+      {isLoading ? <EpisodeSkeleton /> : <Relevance isSearchResult={!!searchTerm} />}
+    </ScrollWrapper>
   )
 }
 
--- src/stores/useSchemaStore/index.ts
+++ src/stores/useSchemaStore/index.ts
@@ -60,12 +60,13 @@ export const useSchemaStore = create<SchemasStore>()(
         const paletteColor = COLORS_PALETTE.find((colorsArray) => colorsArray[1] === i.primary_color)
         const cycleIndex = index % COLORS_PALETTE.length
 
-        const secondaryColor = paletteColor?.[0] ?? COLORS_PALETTE[index]?.[0] ?? COLORS_PALETTE[0][0]
+        const secondaryColor = paletteColor?.[0]
+          ? paletteColor[0]
+          : COLORS_PALETTE[cycleIndex][0] || COLORS_PALETTE[0][0]
 
         return {
           ...i,
-          primary_color: paletteColor ? i.primary_color : COLORS_PALETTE[index]?.[1] ?? COLORS_PALETTE[0][0],
-
+          primary_color: paletteColor ? i.primary_color : COLORS_PALETTE[cycleIndex][1] || COLORS_PALETTE[0][0],
           secondary_color: secondaryColor,
         }
       })
