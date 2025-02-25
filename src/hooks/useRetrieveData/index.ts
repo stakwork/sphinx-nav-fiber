1ee04775a4fd191e78b7ee85cc29a746b6437b3b
Расул <rasul.sahibjanli@gmail.com>
2025-02-25 23:22:42 +0300

    feat: try 2

--- src/hooks/useRetrieveData/index.ts
+++ src/hooks/useRetrieveData/index.ts
@@ -15,13 +15,7 @@ export const useRetrieveData = () => {
 
   const { setSidebarOpen, currentSearch: searchTerm } = useAppStore((s) => s)
 
-  const fetchData = useDataStore((s) => s.fetchData)
-  const setAbortRequests = useDataStore((s) => s.setAbortRequests)
-  const resetData = useDataStore((s) => s.resetData)
-  const addNewNode = useDataStore((s) => s.addNewNode)
-  const finishLoading = useDataStore((s) => s.finishLoading)
-
-  console.log('try')
+  const { fetchData, setAbortRequests, resetData, addNewNode, finishLoading } = useDataStore((s) => s)
 
   useEffect(() => {
     if (initialLoad.current) {
