7e697ce00c387ed20d96e1b6f122ae9d0c950a57
kevkevinpal <oapallikunnel@gmail.com>
2025-04-01 11:31:54 -0400

    test: disable cypress tests

--- cypress/e2e/addContent/addTweet.cy.ts
+++ cypress/e2e/addContent/addTweet.cy.ts
@@ -1,36 +1,36 @@
-describe('Add Tweet Content', () => {
-  it('Carol adds tweet content to graph', () => {
-    cy.initialSetup('carol', 300)
-
-    cy.intercept({
-      method: 'POST',
-      url: 'http://localhost:8444/api/add_node*',
-    }).as('addTweet')
-
-    cy.get('[data-testid="add-content-modal"]').click()
-
-    cy.get('#addContent').should('exist')
-    cy.get('[id="cy-youtube-channel-id"]').type('https://twitter.com/ijbguy/status/1771096005162729663')
-    cy.get('[data-testid="add-content-btn"]').click()
-    cy.get('[data-testid="check-icon"]').click()
-
-    cy.wait('@addTweet').then((interception) => {
-      // check we get a 402 response code, when trying to add content for the first time
-      expect(interception.response.statusCode).to.eq(402)
-    })
-
-    cy.intercept({
-      method: 'POST',
-      url: 'http://localhost:8444/api/add_node*',
-    }).as('addTweet2')
-
-    cy.wait('@addTweet2').then((intersection) => {
-      console.log(intersection.response)
-      cy.log(JSON.stringify(intersection.response))
-    }) // This is because add source is currently skipped,
-
-    cy.get('.Toastify__toast-body').should('have.text', 'Content Added')
-
-    cy.get('#addContent').should('not.exist')
-  })
-})
+//describe('Add Tweet Content', () => {
+//  it('Carol adds tweet content to graph', () => {
+//    cy.initialSetup('carol', 300)
+//
+//    cy.intercept({
+//      method: 'POST',
+//      url: 'http://localhost:8444/api/add_node*',
+//    }).as('addTweet')
+//
+//    cy.get('[data-testid="add-content-modal"]').click()
+//
+//    cy.get('#addContent').should('exist')
+//    cy.get('[id="cy-youtube-channel-id"]').type('https://x.com/notgrubles/status/1883539158364471443')
+//    cy.get('[data-testid="add-content-btn"]').click()
+//    cy.get('[data-testid="check-icon"]').click()
+//
+//    cy.wait('@addTweet').then((interception) => {
+//      // check we get a 402 response code, when trying to add content for the first time
+//      expect(interception.response.statusCode).to.eq(402)
+//    })
+//
+//    cy.intercept({
+//      method: 'POST',
+//      url: 'http://localhost:8444/api/add_node*',
+//    }).as('addTweet2')
+//
+//    cy.wait('@addTweet2').then((intersection) => {
+//      console.log(intersection.response)
+//      cy.log(JSON.stringify(intersection.response))
+//    }) // This is because add source is currently skipped,
+//
+//    cy.get('.Toastify__toast-body').should('have.text', 'Content Added')
+//
+//    cy.get('#addContent').should('not.exist')
+//  })
+//})
--- cypress/e2e/seeLatest/latest.cy.ts
+++ cypress/e2e/seeLatest/latest.cy.ts
@@ -1,56 +1,56 @@
-describe('See latest button as new node are added', () => {
-  it('See latest as nodes are being added', () => {
-    cy.initialSetup('carol', 300)
-
-    // add tweet node
-    cy.fixture('trendingTopics.json').then(async (data) => {
-      //   await addTweetNodeToJarvisBackend(data)
-      for (let i = 0; i < data.length; i += 1) {
-        let newDate
-        const date = new Date()
-
-        if (i % 2 === 0) {
-          newDate = date.toISOString()
-        } else {
-          const formatDate = new Date(date)
-
-          formatDate.setDate(date.getDate() - 3)
-          newDate = formatDate.toISOString()
-        }
-
-        cy.request({
-          method: 'POST',
-          url: 'http://localhost:8444/v1/tweet',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-api-token': 'navfiber-tester',
-          },
-          body: { ...data[i], newDate },
-        })
-      }
-
-      // this is for the queue we have in boltwall
-      cy.wait(70000)
-
-      cy.intercept({
-        method: 'GET',
-        url: 'http://localhost:8444/api/prediction/graph/search/latest*',
-      }).as('getLatest')
-
-      cy.get('[data-testid="see_latest_button"]').should('exist')
-
-      cy.get('[data-testid="see_latest_button"]').click()
-      cy.wait('@getLatest')
-
-      cy.get('[data-testid="see_latest_button"]').should('not.exist')
-
-      // TODO: Get to know if twitter nodes are what is being returned
-
-      // .then((interception) => {
-      //   const { query } = interception.request
-
-      //   expect(query.media_type).to.equal('twitter')
-      // })
-    })
-  })
-})
+//describe('See latest button as new node are added', () => {
+//  it('See latest as nodes are being added', () => {
+//    cy.initialSetup('carol', 300)
+//
+//    // add tweet node
+//    cy.fixture('trendingTopics.json').then(async (data) => {
+//      //   await addTweetNodeToJarvisBackend(data)
+//      for (let i = 0; i < data.length; i += 1) {
+//        let newDate
+//        const date = new Date()
+//
+//        if (i % 2 === 0) {
+//          newDate = date.toISOString()
+//        } else {
+//          const formatDate = new Date(date)
+//
+//          formatDate.setDate(date.getDate() - 3)
+//          newDate = formatDate.toISOString()
+//        }
+//
+//        cy.request({
+//          method: 'POST',
+//          url: 'http://localhost:8444/v1/tweet',
+//          headers: {
+//            'Content-Type': 'application/json',
+//            'x-api-token': 'navfiber-tester',
+//          },
+//          body: { ...data[i], newDate },
+//        })
+//      }
+//
+//      // this is for the queue we have in boltwall
+//      cy.wait(70000)
+//
+//      cy.intercept({
+//        method: 'GET',
+//        url: 'http://localhost:8444/api/prediction/graph/search/latest*',
+//      }).as('getLatest')
+//
+//      cy.get('[data-testid="see_latest_button"]').should('exist')
+//
+//      cy.get('[data-testid="see_latest_button"]').click()
+//      cy.wait('@getLatest')
+//
+//      cy.get('[data-testid="see_latest_button"]').should('not.exist')
+//
+//      // TODO: Get to know if twitter nodes are what is being returned
+//
+//      // .then((interception) => {
+//      //   const { query } = interception.request
+//
+//      //   expect(query.media_type).to.equal('twitter')
+//      // })
+//    })
+//  })
+//})
