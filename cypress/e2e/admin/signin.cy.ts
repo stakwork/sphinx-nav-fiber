4c9801e9656f88166080da9bd17cf7d04bdd9ea1
kevkevin <oapallikunnel@gmail.com>
2025-04-01 10:55:58 -0500

    test: disable cypress tests (#2823)

--- cypress/e2e/admin/signin.cy.ts
+++ cypress/e2e/admin/signin.cy.ts
@@ -1,35 +1,35 @@
-describe('Admin Login', () => {
-  it('Admin uses the enable function', () => {
-    const username = 'alice'
-
-    cy.initialSetup(username, 50)
-
-    cy.intercept({
-      method: 'POST',
-      url: 'http://localhost:8444/api/about*',
-    }).as('updateAbout')
-
-    const title = `Testing NavFiber`
-    const description = 'Testing Graph Description'
-
-    // Open settings modal
-    cy.get('div[data-testid="settings-modal"]').should('be.visible').click()
-
-    // Asserting the settings label text
-    cy.get('[data-testid="setting-label"]').should('have.text', 'Admin Settings')
-
-    // Efficiently interact with the about title
-    cy.get('#cy-about-title-id').should('be.visible').click().type('{selectAll}').type(title)
-
-    // Efficiently interact with the about description
-    cy.get('#cy-about-id').should('be.visible').click().type('{selectAll}').type(description)
-
-    // Submit the form
-    cy.get('#add-node-submit-cta').click()
-    cy.wait('@updateAbout')
-
-    cy.wait(1000)
-
-    cy.get('.title').should('have.text', title)
-  })
-})
+//describe('Admin Login', () => {
+//  it('Admin uses the enable function', () => {
+//    const username = 'alice'
+//
+//    cy.initialSetup(username, 50)
+//
+//    cy.intercept({
+//      method: 'POST',
+//      url: 'http://localhost:8444/api/about*',
+//    }).as('updateAbout')
+//
+//    const title = `Testing NavFiber`
+//    const description = 'Testing Graph Description'
+//
+//    // Open settings modal
+//    cy.get('div[data-testid="settings-modal"]').should('be.visible').click()
+//
+//    // Asserting the settings label text
+//    cy.get('[data-testid="setting-label"]').should('have.text', 'Admin Settings')
+//
+//    // Efficiently interact with the about title
+//    cy.get('#cy-about-title-id').should('be.visible').click().type('{selectAll}').type(title)
+//
+//    // Efficiently interact with the about description
+//    cy.get('#cy-about-id').should('be.visible').click().type('{selectAll}').type(description)
+//
+//    // Submit the form
+//    cy.get('#add-node-submit-cta').click()
+//    cy.wait('@updateAbout')
+//
+//    cy.wait(1000)
+//
+//    cy.get('.title').should('have.text', title)
+//  })
+//})
