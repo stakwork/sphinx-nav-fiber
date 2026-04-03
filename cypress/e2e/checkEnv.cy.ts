b9b812e91091396b22c770c4a18acc6936e8fd6f
Rassl <Rassl@users.noreply.github.com>
2026-04-03 10:47:26 +0000

    Generated with Hive: Remove all e2e test files from Cypress

--- cypress/e2e/checkEnv.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Checking test enviroment', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
-  it('Using "yarn start-e2e" for tests running', () => {
-    cy.get('#e2e-check').should('be.exist')
-  })
-})
